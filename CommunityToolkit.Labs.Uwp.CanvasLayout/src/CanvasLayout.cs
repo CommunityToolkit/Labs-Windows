8a83f55fde9953a5723f16e17c42b4c00ef30afd
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2021-10-14 16:36:33 -0700

    Add File Header Analyzer and License Files

--- CommunityToolkit.Labs.Uwp.CanvasLayout/src/CanvasLayout.cs
+++ CommunityToolkit.Labs.Uwp.CanvasLayout/src/CanvasLayout.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.UI.Xaml.Controls;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.UI.Xaml.Controls;
 
 namespace CommunityToolkit.Labs.Uwp
 {
