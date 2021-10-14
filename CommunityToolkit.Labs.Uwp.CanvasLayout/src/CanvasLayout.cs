660cf4424d5746c0e0b7a091a2a9c43ca2a6182a
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2021-10-14 16:27:32 -0700

    Add code project
    Restructure to separate out sample from source in own directory

---
+++ CommunityToolkit.Labs.Uwp.CanvasLayout/src/CanvasLayout.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace CommunityToolkit.Labs.Uwp
+{
+    public class CanvasLayout : VirtualizingLayout
+    {
+    }
+}
---
+++ CommunityToolkit.Labs.Uwp.CanvasLayout/src/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
