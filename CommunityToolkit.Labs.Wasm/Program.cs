91d8d7b75bd1996d6151c49003c90dcebfd835b8
Arlo Godfrey <arlo.godfrey@outlook.com>
2021-12-07 18:56:20 -0600

    Made existing projects Uno-capable, added Wasm projects, added root "all-sample" apps, organization and cleanup.

---
+++ CommunityToolkit.Labs.Wasm/Program.cs
@@ -0,0 +1,18 @@
+using CommunityToolkit.Labs.Shared;
+using System;
+using Windows.UI.Xaml;
+
+namespace CommunityToolkit.Labs.Wasm
+{
+	public class Program
+	{
+		private static App _app;
+
+		static int Main(string[] args)
+		{
+			Windows.UI.Xaml.Application.Start(_ => _app = new App());
+
+			return 0;
+		}
+	}
+}
