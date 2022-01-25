10c5e864485e6a4a91d28137e7ea520e67cad0c9
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-01-25 17:17:02 -0600

    Added CrawlBy helper

--- Common/CommunityToolkit.Labs.Core/Generators/GeneratorExtensions.cs
+++ Common/CommunityToolkit.Labs.Core/Generators/GeneratorExtensions.cs
@@ -28,6 +28,31 @@ namespace CommunityToolkit.Labs.Core.Generators
         }
 
         /// <summary>
+        /// Crawls an object tree for nested properties of the same type and returns the first instance that matches the <paramref name="filterPredicate"/>. 
+        /// </summary>
+        /// <remarks>
+        /// Does not filter against or return the <paramref name="root"/> object.
+        /// </remarks>
+        public static T? CrawlBy<T>(this T? root, Func<T?, T?> selectPredicate, Func<T?, bool> filterPredicate)
+        {
+            crawl:
+            var current = selectPredicate(root);
+
+            if (filterPredicate(current))
+            {
+                return current;
+            }
+
+            if (current is null)
+            {
+                return default;
+            }
+
+            root = current;
+            goto crawl;
+        }
+
+        /// <summary>
         /// Reconstructs an attribute instance as the given type.
         /// </summary>
         /// <typeparam name="T">The attribute type to create.</typeparam>
