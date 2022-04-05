e471c4539b023a286953cc83e2c56e1a452a30ac
Matt Lacey <matt@mrlacey.co.uk>
2022-04-05 19:11:00 +0100

    Add unit tests (#61)
    * Add unit tests
    
    For #48
    
    * Update Tests/CommunityToolkit.Labs.UnitTests.Uwp/CommunityToolkit.Labs.UnitTests.Uwp.csproj
    
    Co-authored-by: Michael Hawker MSFT (XAML Llama) <24302614+michael-hawker@users.noreply.github.com>
    
    * Standardize casing of directory names
    
    * shorten "Labs.WinUI.CanvasLayout.Tests"
    
    to "CanvasLayout.Tests"
    
    * Shorten experiment test project name
    
    * removed duplicated default Windows app assets
    
    * Remove non-essential assets from test projects
    
    * Share Asset files in the test host projects
    
    * test projects to use common/shared asset files
    
    * standardize XAML formatting
    
    * Do not set passive flag if not running xstyler passively
    
    * remove experiment specific sample heads from "All" solution
    
    and remove folder as will only eve have a single item in it.
    
    * remove unneeded experiment head project from WASM solution
    
    * update test related package dependencies
    
    Co-authored-by: Michael Hawker MSFT (XAML Llama) <24302614+michael-hawker@users.noreply.github.com>

---
+++ Labs/CanvasLayout/Tests/CanvasLayout.UnitTests.Uwp/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CanvasLayout.UnitTests.Uwp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CanvasLayout.UnitTests.Uwp")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyMetadata("TargetPlatform","UAP")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
