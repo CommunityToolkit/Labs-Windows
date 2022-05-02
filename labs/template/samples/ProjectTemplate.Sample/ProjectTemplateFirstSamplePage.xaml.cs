71a17ee613ddcc2f186fbe71cc522c889848bb69
Matt Lacey <matt@mrlacey.co.uk>
2022-05-02 20:48:49 +0100

    move template into the labs folder
    so that relative paths are the right depths

--- labs/template/samples/ProjectTemplate.Sample/ProjectTemplateFirstSamplePage.xaml.cs
+++ labs/template/samples/ProjectTemplate.Sample/ProjectTemplateFirstSamplePage.xaml.cs
@@ -2,13 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using CommunityToolkit.Labs.Core.SourceGenerators;
 using CommunityToolkit.Labs.Core.SourceGenerators.Attributes;
-using System.Runtime.InteropServices.WindowsRuntime;
 
 //-:cnd:noEmit
 #if !WINAPPSDK
