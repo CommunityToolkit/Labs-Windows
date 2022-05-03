ef0b41ae7d3bf8208f1fb0e1cda5ba2ce03fa659
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2022-05-03 14:06:27 -0700

    Add additional Markdown Source Generator Tests

--- common/CommunityToolkit.Labs.Core.SourceGenerators.Tests/CommunityToolkit.Labs.Core.SourceGenerators.Tests/ToolkitSampleMetadataTests.Documentation.cs
+++ common/CommunityToolkit.Labs.Core.SourceGenerators.Tests/CommunityToolkit.Labs.Core.SourceGenerators.Tests/ToolkitSampleMetadataTests.Documentation.cs
@@ -42,5 +42,99 @@ namespace CommunityToolkit.Labs.Core.SourceGenerators.Tests
 
             VerifyGeneratedDiagnostics<ToolkitSampleMetadataGenerator>(SimpleSource, markdown, DiagnosticDescriptors.MarkdownYAMLFrontMatterException.Id, DiagnosticDescriptors.SampleNotReferencedInMarkdown.Id);
         }
+
+        [DataRow(1, DisplayName = "Title")]
+        [DataRow(3, DisplayName = "Description")]
+        [DataRow(4, DisplayName = "Keywords")]
+        [DataRow(7, DisplayName = "Category")]
+        [DataRow(8, DisplayName = "Subcategory")]
+        [TestMethod]
+        public void MissingFrontMatterField(int removeline)
+        {
+            string markdown = @"---
+title: Canvas Layout
+author: mhawker
+description: A canvas-like VirtualizingLayout for use in an ItemsRepeater
+keywords: CanvasLayout, ItemsRepeater, VirtualizingLayout, Canvas, Layout, Panel, Arrange
+dev_langs:
+    - csharp
+category: Controls
+subcategory: Layout
+---
+# This is some test documentation...
+> [!SAMPLE Sample]
+Without any front matter.";
+
+            // Remove the field we want to test is missing.
+            var lines = markdown.Split('\n').ToList();
+            lines.RemoveAt(removeline);
+            markdown = String.Join('\n', lines);
+
+            VerifyGeneratedDiagnostics<ToolkitSampleMetadataGenerator>(SimpleSource, markdown, DiagnosticDescriptors.MarkdownYAMLFrontMatterMissingField.Id, DiagnosticDescriptors.SampleNotReferencedInMarkdown.Id); // We won't see the sample reference as we bail out when the front matter fails to be complete...
+        }
+
+        [TestMethod]
+        public void MarkdownInvalidSampleReference()
+        {
+            string markdown = @"---
+title: Canvas Layout
+author: mhawker
+description: A canvas-like VirtualizingLayout for use in an ItemsRepeater
+keywords: CanvasLayout, ItemsRepeater, VirtualizingLayout, Canvas, Layout, Panel, Arrange
+dev_langs:
+    - csharp
+category: Controls
+subcategory: Layout
+---
+# This is some test documentation...
+> [!SAMPLE SampINVALIDle]
+Without any front matter.
+";
+
+            VerifyGeneratedDiagnostics<ToolkitSampleMetadataGenerator>(SimpleSource, markdown,
+                DiagnosticDescriptors.MarkdownSampleIdNotFound.Id,
+                DiagnosticDescriptors.SampleNotReferencedInMarkdown.Id);
+        }
+
+        [TestMethod]
+        public void DocumentationMissingSample()
+        {
+            string markdown = @"---
+title: Canvas Layout
+author: mhawker
+description: A canvas-like VirtualizingLayout for use in an ItemsRepeater
+keywords: CanvasLayout, ItemsRepeater, VirtualizingLayout, Canvas, Layout, Panel, Arrange
+dev_langs:
+    - csharp
+category: Controls
+subcategory: Layout
+---
+# This is some test documentation...
+Without any front matter.";
+
+            VerifyGeneratedDiagnostics<ToolkitSampleMetadataGenerator>(SimpleSource, markdown,
+                DiagnosticDescriptors.DocumentationHasNoSamples.Id,
+                DiagnosticDescriptors.SampleNotReferencedInMarkdown.Id);
+        }
+
+        [TestMethod]
+        public void DocumentationValid()
+        {
+            string markdown = @"---
+title: Canvas Layout
+author: mhawker
+description: A canvas-like VirtualizingLayout for use in an ItemsRepeater
+keywords: CanvasLayout, ItemsRepeater, VirtualizingLayout, Canvas, Layout, Panel, Arrange
+dev_langs:
+    - csharp
+category: Controls
+subcategory: Layout
+---
+# This is some test documentation...
+Without any front matter.
+> [!SAMPLE Sample]";
+
+            VerifyGeneratedDiagnostics<ToolkitSampleMetadataGenerator>(SimpleSource, markdown);
+        }
     }
 }
