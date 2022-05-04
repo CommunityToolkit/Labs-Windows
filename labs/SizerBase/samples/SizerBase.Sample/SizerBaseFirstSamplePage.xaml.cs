47acf96afd5df417a98b44165e43502ca9cf26e5
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2022-05-04 16:08:58 -0700

    Initial SizerBase template commit

---
+++ labs/SizerBase/samples/SizerBase.Sample/SizerBaseFirstSamplePage.xaml.cs
@@ -0,0 +1,53 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommunityToolkit.Labs.Core.SourceGenerators;
+using CommunityToolkit.Labs.Core.SourceGenerators.Attributes;
+
+#if !WINAPPSDK
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+#else
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+#endif
+
+
+namespace SizerBase.Sample
+{
+    [ToolkitSampleBoolOption("IsTextVisible", "IsVisible", true)]
+
+    [ToolkitSampleMultiChoiceOption("TextForeground", label: "Teal", value: "#0ddc8c", title: "Text foreground")]
+    [ToolkitSampleMultiChoiceOption("TextForeground", label: "Sand", value: "#e7a676")]
+    [ToolkitSampleMultiChoiceOption("TextForeground", label: "Dull green", value: "#5d7577")]
+
+    [ToolkitSampleMultiChoiceOption("TextSize", label: "Small", value: "12", title: "Text size")]
+    [ToolkitSampleMultiChoiceOption("TextSize", label: "Normal", value: "16")]
+    [ToolkitSampleMultiChoiceOption("TextSize", label: "Big", value: "32")]
+
+    [ToolkitSampleMultiChoiceOption("TextFontFamily", label: "Segoe UI", value: "Segoe UI")]
+    [ToolkitSampleMultiChoiceOption("TextFontFamily", label: "Arial", value: "Arial")]
+    [ToolkitSampleMultiChoiceOption("TextFontFamily", label: "Consolas", value: "Consolas")]
+
+    [ToolkitSample(id: nameof(SizerBaseFirstSamplePage), "Simple Options", description: "A sample page for showing how to do simple options.")]
+    public sealed partial class SizerBaseFirstSamplePage : Page
+    {
+        public SizerBaseFirstSamplePage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
