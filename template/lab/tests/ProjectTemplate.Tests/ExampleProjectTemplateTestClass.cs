dafeb0edb5c6d34c1997c0ea16cbffccf831bf42
Matt Lacey <matt@mrlacey.co.uk>
2022-05-20 21:08:55 +0100

    Increase the namespaces referenced in the example test file
    This list is based on what's most commonly used in existing toolkit tests.

--- template/lab/tests/ProjectTemplate.Tests/ExampleProjectTemplateTestClass.cs
+++ template/lab/tests/ProjectTemplate.Tests/ExampleProjectTemplateTestClass.cs
@@ -6,6 +6,26 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CommunityToolkit.Labs.WinUI;
+
+//-:cnd:noEmit
+#if !WINAPPSDK
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
+#else
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Markup;
+using Microsoft.UI.Xaml.Media;
+#endif
+//+:cnd:noEmit
 
 namespace ProjectTemplate.Tests
 {
