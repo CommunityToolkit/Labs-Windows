aba0670db9bc96d57809ff77390da6b518ea723c
Arlo <arlo.godfrey@outlook.com>
2022-06-06 17:13:34 -0500

    Accommodate WinUI 2/3 dependencies per-experiment (#139)
    * Added props files for WinUI 2 and WinUI 3 dependencies
    
    * Fixed an issue where WinUI version had to be swapped for Uwp or WinAppSdk to get dependencies
    
    * Fixed UWP TargetFramework check
    
    * Update template/lab/src/Labs.WinUI.Dependencies.props
    
    Co-authored-by: Michael Hawker MSFT (XAML Llama) <24302614+michael-hawker@users.noreply.github.com>
    
    * Fixed invalid WinAppSdk check
    
    * Rename UseWinUI to WinUIMajorVersion
    
    * Added note for NS2.0 dependencies
    
    * Update labs/CanvasLayout/src/Dependencies.WinUI3.props
    
    * Moved lab WinUI dependency data to existing Uno props file
    
    Co-authored-by: Michael Hawker MSFT (XAML Llama) <24302614+michael-hawker@users.noreply.github.com>

--- common/Scripts/UseUnoWinUI.ps1
+++ common/Scripts/UseUnoWinUI.ps1
@@ -11,25 +11,27 @@ Param (
 & git config advice.useCoreFSMonitorConfig false;
 
 if ($allowGitChanges.IsPresent) {
-    Write-Warning "Changes to the default Uno package references in Labs can now be committed. Run this command again without the -allowGitChanges flag to disable committing further changes.";
+    Write-Warning "Changes to the default Uno package settings in Labs can now be committed. Run this command again without the -allowGitChanges flag to disable committing further changes.";
     git update-index --no-assume-unchanged ../Labs.Uno.props
 }
 else {
-    Write-Output "Changes the default Uno package references in Labs are now suppressed. To switch branches, run git reset --hard with a clean working tree.";
+    Write-Output "Changes to the default Uno package settings in Labs are now suppressed. To switch branches, run git reset --hard with a clean working tree.";
     git update-index --assume-unchanged ../Labs.Uno.props
 }
 
 $fileContents = Get-Content -Path ../Labs.Uno.props
 
 if ($targets -eq "3") {
+    $fileContents = $fileContents -replace '<WinUIMajorVersion>2</WinUIMajorVersion>', '<WinUIMajorVersion>3</WinUIMajorVersion>';
+    $fileContents = $fileContents -replace '<PackageIdVariant>Uwp</PackageIdVariant>', '<PackageIdVariant>WinUI</PackageIdVariant>';
     $fileContents = $fileContents -replace 'Uno.UI', 'Uno.WinUI';
-    $fileContents = $fileContents -replace 'Uwp', 'WinUI';
     $fileContents = $fileContents -replace '\$\(DefineConstants\);', '$(DefineConstants);WINAPPSDK;';
 }
 
 if ($targets -eq "2") {
+    $fileContents = $fileContents -replace '<WinUIMajorVersion>3</WinUIMajorVersion>', '<WinUIMajorVersion>2</WinUIMajorVersion>';
+    $fileContents = $fileContents -replace '<PackageIdVariant>WinUI</PackageIdVariant>', '<PackageIdVariant>Uwp</PackageIdVariant>';
     $fileContents = $fileContents -replace 'Uno.WinUI', 'Uno.UI';
-    $fileContents = $fileContents -replace 'WinUI', 'Uwp';
     $fileContents = $fileContents -replace 'WINAPPSDK;', '';
 }
 
