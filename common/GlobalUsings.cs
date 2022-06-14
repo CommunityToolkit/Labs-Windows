0eddb4cf4b8c64794c63134973c88fd7ac7e44bf
Matt Lacey <matt@mrlacey.co.uk>
2022-06-14 23:22:15 +0100

    Add link to docs explaining global using directives

--- common/GlobalUsings.cs
+++ common/GlobalUsings.cs
@@ -1,4 +1,6 @@
 
+// To learn more about this file see https://docs.microsoft.com/en-gb/dotnet/csharp/language-reference/keywords/using-directive#global-modifier
+
 global using System;
 global using System.Collections.Generic;
 global using System.Collections.Immutable;
