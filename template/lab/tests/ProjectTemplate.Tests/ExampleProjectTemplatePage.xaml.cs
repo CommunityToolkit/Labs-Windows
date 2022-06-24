37476b4c4b92930a09bd86edcc414f5fce0ee93a
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-06-24 13:47:05 -0500

    Removed unused usings

--- template/lab/tests/ProjectTemplate.Tests/ExampleProjectTemplatePage.xaml.cs
+++ template/lab/tests/ProjectTemplate.Tests/ExampleProjectTemplatePage.xaml.cs
@@ -6,26 +6,6 @@ using System;
 using System.Collections.Generic;
 using Windows.Foundation;
 
-// TODO: remove any unneeded namespaces before creating a PR
-//-:cnd:noEmit
-#if !WINAPPSDK
-using Windows.UI;
-using Windows.UI.Core;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Markup;
-using Windows.UI.Xaml.Media;
-#else
-using Microsoft.UI;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Markup;
-using Microsoft.UI.Xaml.Media;
-#endif
-//+:cnd:noEmit
-
 namespace ProjectTemplate.Tests;
 
 /// <summary>
