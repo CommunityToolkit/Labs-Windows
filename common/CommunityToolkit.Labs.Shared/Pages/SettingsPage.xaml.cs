76a849f2bc34bc963b07fcb6aad89f385dca62d9
Niels Laute <niels.laute@live.nl>
2022-06-28 14:14:14 +0200

    Remove usings

--- common/CommunityToolkit.Labs.Shared/Pages/SettingsPage.xaml.cs
+++ common/CommunityToolkit.Labs.Shared/Pages/SettingsPage.xaml.cs
@@ -2,28 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using CommunityToolkit.Labs.Core;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation.Collections;
-
-#if !WINAPPSDK
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-#else
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-#endif
-
 namespace CommunityToolkit.Labs.Shared.Pages
 {
     /// <summary>
