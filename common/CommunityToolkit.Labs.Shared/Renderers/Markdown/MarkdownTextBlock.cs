828fed37f068601b1c15b13f2bbee5c8bda110c0
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2022-06-30 15:52:48 -0700

    WIP - Doesn't work, trying to add Toolkit refence to Sample App

---
+++ common/CommunityToolkit.Labs.Shared/Renderers/Markdown/MarkdownTextBlock.cs
@@ -0,0 +1,31 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#if HAS_UNO
+    #if WINAPPSDK
+    using ToolkitMTB = Microsoft.UI.Xaml.DependencyObject;
+    #else
+    using ToolkitMTB = Windows.UI.Xaml.DependencyObject;
+    #endif
+#else
+    #if WINAPPSDK
+    using ToolkitMTB = CommunityToolkit.WinUI.UI.Controls.MarkdownTextBlock;
+    #else
+    using ToolkitMTB = Microsoft.Toolkit.Uwp.UI.Controls.MarkdownTextBlock;
+    #endif
+#endif
+
+namespace CommunityToolkit.Labs.Shared.Renderers;
+
+/// <summary>
+/// Provide an abstraction around the Toolkit MarkdownTextBlock for both UWP and WinUI 3 in the same namespace (until 8.0) as well as a polyfill for WebAssembly/WASM.
+/// </summary>
+public partial class MarkdownTextBlock : ToolkitMTB
+{
+#if HAS_UNO
+    public string? Text;
+    public bool? IsTextSelectionEnabled;
+    public TextWrapping? TextWrapping;
+#endif
+}
