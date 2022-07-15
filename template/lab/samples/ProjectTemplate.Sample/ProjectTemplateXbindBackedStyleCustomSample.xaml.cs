6e9b204aa741bcf9d143c199758c308e0f56f69f
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2022-07-15 10:20:52 -0700

    Rename ProjectTemplate namespace to include suffix of Experiment
    Fixes #194

--- template/lab/samples/ProjectTemplate.Sample/ProjectTemplateXbindBackedStyleCustomSample.xaml.cs
+++ template/lab/samples/ProjectTemplate.Sample/ProjectTemplateXbindBackedStyleCustomSample.xaml.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace ProjectTemplate.Sample;
+namespace ProjectTemplateExperiment.Samples;
 
 [ToolkitSampleBoolOption("IsTextVisible", "IsVisible", true)]
 // Single values without a colon are used for both label and value.
