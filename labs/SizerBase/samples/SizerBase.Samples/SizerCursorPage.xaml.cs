7c7217d462352704daa0fca0b1d93c188e84b294
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2022-07-15 19:56:09 -0700

    Regenerate SizerBase Experiment with new Template

--- labs/SizerBase/samples/SizerBase.Samples/SizerCursorPage.xaml.cs
+++ labs/SizerBase/samples/SizerBase.Samples/SizerCursorPage.xaml.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace SizerBase.Sample;
+namespace SizerBaseExperiment.Samples;
 
 [ToolkitSample(id: nameof(SizerCursorPage), "Custom Mouse Cursor", description: "Shows how to change the cursor of a Sizer control.")]
 public sealed partial class SizerCursorPage : Page
