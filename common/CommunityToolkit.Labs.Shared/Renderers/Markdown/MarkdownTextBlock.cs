7603ed5a6710ec799ec49156e271e15910d5b7d3
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2022-07-17 16:52:24 -0700

    Add initial MarkdownTextBlock to sample app for UWP/WinAppSDK

---
+++ common/CommunityToolkit.Labs.Shared/Renderers/Markdown/MarkdownTextBlock.cs
@@ -0,0 +1,26 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#if HAS_UNO
+    #if WINAPPSDK
+    using ToolkitMTB = Microsoft.UI.Xaml.Controls.TextBlock;
+    #else
+    using ToolkitMTB = Windows.UI.Xaml.Controls.TextBlock;
+    #endif
+#else
+    #if WINAPPSDK
+    using ToolkitMTB = CommunityToolkit.WinUI.UI.Controls.MarkdownTextBlock;
+    #else
+    using ToolkitMTB = Microsoft.Toolkit.Uwp.UI.Controls.MarkdownTextBlock;
+    #endif
+#endif
+
+namespace CommunityToolkit.Labs.Shared.Renderers;
+
+/// <summary>
+/// Provide an abstraction around the Toolkit MarkdownTextBlock for both UWP and WinUI 3 in the same namespace (until 8.0) as well as a polyfill for WebAssembly/WASM.
+/// </summary>
+public partial class MarkdownTextBlock : ToolkitMTB
+{
+}
