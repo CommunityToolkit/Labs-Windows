2a916c8410ebb26c9be29f49fa2c3412d2744453
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-07-18 18:22:53 -0500

    Fixed false TFM errors appearing in Visual Studio when restoring packages

--- common/Scripts/UseUnoWinUI.ps1
+++ common/Scripts/UseUnoWinUI.ps1
@@ -1,38 +1,54 @@
 Param (
     [Parameter(HelpMessage = "The WinUI version to use when building an Uno head.", Mandatory = $true)]
     [ValidateSet('2', '3')]
-    [string]$targets,
+    [string]$winUIMajorVersion,
     
-    [Parameter(HelpMessage = "Disables suppressing changes to the Labs.Uno.props file in git, allowing changes to be committed.")] 
+    [Parameter(HelpMessage = "Disables suppressing changes to the Labs.Uno.props and Labs.Head.Uno.props files in git, allowing changes to be committed.")] 
     [switch]$allowGitChanges = $false
 )
 
 # Suppress git warning "core.useBuiltinFSMonitor will be deprecated soon; use core.fsmonitor instead"
 & git config advice.useCoreFSMonitorConfig false;
 
+function SupressOrAllowChanges([string] $filePath) {
+    if ($allowGitChanges.IsPresent) {
+        git update-index --no-assume-unchanged $filePath
+    }
+    else {
+        git update-index --assume-unchanged $filePath
+    }
+}
+
+
+function ApplyWinUISwap([string] $filePath) {
+    SupressOrAllowChanges $filePath;
+
+    $fileContents = Get-Content -Path $filePath;
+    
+    if ($winUIMajorVersion -eq "3") {
+        $fileContents = $fileContents -replace '<WinUIMajorVersion>2</WinUIMajorVersion>', '<WinUIMajorVersion>3</WinUIMajorVersion>';
+        $fileContents = $fileContents -replace '<PackageIdVariant>Uwp</PackageIdVariant>', '<PackageIdVariant>WinUI</PackageIdVariant>';
+        $fileContents = $fileContents -replace 'Uno.UI', 'Uno.WinUI';
+        $fileContents = $fileContents -replace '\$\(DefineConstants\);', '$(DefineConstants);WINAPPSDK;';
+    }
+
+    if ($winUIMajorVersion -eq "2") {    
+        $fileContents = $fileContents -replace '<WinUIMajorVersion>3</WinUIMajorVersion>', '<WinUIMajorVersion>2</WinUIMajorVersion>';
+        $fileContents = $fileContents -replace '<PackageIdVariant>WinUI</PackageIdVariant>', '<PackageIdVariant>Uwp</PackageIdVariant>';
+        $fileContents = $fileContents -replace 'Uno.WinUI', 'Uno.UI';
+        $fileContents = $fileContents -replace 'WINAPPSDK;', '';
+    }
+
+    Set-Content -Force -Path $filePath -Value $fileContents;
+}
+
+
+ApplyWinUISwap $PSScriptRoot/../Labs.Head.Uno.props
+ApplyWinUISwap $PSScriptRoot/../Labs.Uno.props
+
 if ($allowGitChanges.IsPresent) {
     Write-Warning "Changes to the default Uno package settings in Labs can now be committed. Run this command again without the -allowGitChanges flag to disable committing further changes.";
-    git update-index --no-assume-unchanged ../Labs.Uno.props
 }
 else {
     Write-Output "Changes to the default Uno package settings in Labs are now suppressed. To switch branches, run git reset --hard with a clean working tree.";
-    git update-index --assume-unchanged ../Labs.Uno.props
 }
-
-$fileContents = Get-Content -Path ../Labs.Uno.props
-
-if ($targets -eq "3") {
-    $fileContents = $fileContents -replace '<WinUIMajorVersion>2</WinUIMajorVersion>', '<WinUIMajorVersion>3</WinUIMajorVersion>';
-    $fileContents = $fileContents -replace '<PackageIdVariant>Uwp</PackageIdVariant>', '<PackageIdVariant>WinUI</PackageIdVariant>';
-    $fileContents = $fileContents -replace 'Uno.UI', 'Uno.WinUI';
-    $fileContents = $fileContents -replace '\$\(DefineConstants\);', '$(DefineConstants);WINAPPSDK;';
-}
-
-if ($targets -eq "2") {
-    $fileContents = $fileContents -replace '<WinUIMajorVersion>3</WinUIMajorVersion>', '<WinUIMajorVersion>2</WinUIMajorVersion>';
-    $fileContents = $fileContents -replace '<PackageIdVariant>WinUI</PackageIdVariant>', '<PackageIdVariant>Uwp</PackageIdVariant>';
-    $fileContents = $fileContents -replace 'Uno.WinUI', 'Uno.UI';
-    $fileContents = $fileContents -replace 'WINAPPSDK;', '';
-}
-
-Set-Content -Force -Path ../Labs.Uno.props -Value $fileContents;
