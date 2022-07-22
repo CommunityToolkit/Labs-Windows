55af407e8b72034b961974f0ce4710d4b6291f99
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2022-07-21 18:09:05 -0700

    Update namespaces based on refactor that had occurred in main.

--- common/CommunityToolkit.Labs.Tests.Shared/SimulateInput.cs
+++ common/CommunityToolkit.Labs.Tests.Shared/SimulateInput.cs
@@ -2,24 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Windows.UI.Input.Preview.Injection;
-using Windows.Foundation;
 
-#if !WINAPPSDK
-using Windows.UI.Input;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Media;
-#else
-using Microsoft.UI.Input;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Media;
-#endif
-
-namespace CommunityToolkit.Labs.UnitTests;
+namespace CommunityToolkit.Labs.Tests;
 
 public static class SimulateInput
 {
--- labs/SizerBase/tests/SizerBase.Tests/TouchInjectionTest.xaml.cs
+++ labs/SizerBase/tests/SizerBase.Tests/TouchInjectionTest.xaml.cs
@@ -2,25 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#if !WINAPPSDK
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-#else
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-#endif
-
-namespace SizerBase.Tests;
+namespace SizerBaseExperiment.Tests;
 
 /// <summary>
 /// An empty page that can be used on its own or navigated to within a Frame.
