1732f19c91348f7b94aeafc6620075079205b900
Niels Laute <niels.laute@live.nl>
2022-08-01 13:54:23 +0200

    Adding sampleref

--- labs/SettingsControls/samples/SettingsControls.Samples/SettingsExpanderSample.xaml.cs
+++ labs/SettingsControls/samples/SettingsControls.Samples/SettingsExpanderSample.xaml.cs
@@ -4,8 +4,8 @@
 
 namespace SettingsControlsExperiment.Samples;
 
-//[ToolkitSampleBoolOption("IsCardEnabled", "IsEnabled", true)]
-//[ToolkitSampleBoolOption("IsExpanderExpanded", "IsExpanded", false)]
+[ToolkitSampleBoolOption("IsCardEnabled", "IsEnabled", true)]
+[ToolkitSampleBoolOption("IsExpanderExpanded", "IsExpanded", false)]
 // Single values without a colon are used for both label and value.
 // To provide a different label for the value, separate with a colon surrounded by a single space on both sides ("label : value").
 //[ToolkitSampleMultiChoiceOption("TextSize", title: "Text size", "Small : 12", "Normal : 16", "Big : 32")]
