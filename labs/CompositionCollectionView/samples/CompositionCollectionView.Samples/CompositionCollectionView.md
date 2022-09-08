d9b54e38a3d8965ebcbe4d41165372990bc0d3b7
Arcadio Garcia Salvadores <arcadiog@microsoft.com>
2022-09-07 21:06:15 -0700

    Sample text

--- labs/CompositionCollectionView/samples/CompositionCollectionView.Samples/CompositionCollectionView.md
+++ labs/CompositionCollectionView/samples/CompositionCollectionView.Samples/CompositionCollectionView.md
@@ -1,7 +1,7 @@
 ---
 title: CompositionCollectionView
-author: githubaccount
-description: TODO: Your experiment's description here
+author: arcadiogarcia
+description: A composition-driven collection view with fully customizable look and behavior
 keywords: CompositionCollectionView, Control, Layout
 dev_langs:
   - csharp
@@ -18,7 +18,30 @@ subcategory: Layout
 # CompositionCollectionView
 
 For more information about this experiment see:
-- Discussion: TODO: PASTE LINK HERE
-- Issue: TODO: PASTE LINK HERE
+- Discussion: https://github.com/CommunityToolkit/WindowsCommunityToolkit/discussions/4565
+- Issue: https://github.com/CommunityToolkit/Labs-Windows/issues/135
 
-TODO: Fill in information about this experiment and how to get started here...
+CompositionCollectionView is a control that can host and position a collection of elements using developer-defined layouts based on the composition animation APIs
+This facilitates the task of creating complex, performant layouts that are integrated with touch gestures, allowing to reuse logic across them.
+
+A layout can be as simple as this one, which calculates an static position for each element according to its properties:
+
+> [!SAMPLE CompositionCollectionViewFirstSamplePage]
+
+While a CompositionCollectionView is only using one layout at a time, it can transition to any other layout on demand, implicitly animating the elements if desired.
+
+This sample shows how easy it is to transition between two unrelated layouts:
+
+> [!SAMPLE SwitchLayoutsSample]
+
+Behaviors can be used to define interactions, gestures or any other custom logic that might be shared across layouts. The built in InteractionTrackerGesture simplifies the task of integrating touch into a layout and defining layout-independent touch gestures.
+
+This sample shows a scrollable list of elements that follow a custom path:
+
+> [!SAMPLE InteractionTrackerSample]
+
+Layouts can be as complicated as you want, your creativity is the limit!
+
+This sample is inspired by the classic Maze 3d screensaver. The user can move around the maze with touch gestures (collisions with the inner walls are ommited) and will transition to an overhead view when they touch the smiley face.
+
+> [!SAMPLE MazeSample]
--- labs/CompositionCollectionView/samples/CompositionCollectionView.Samples/MazeSample.xaml.cs
+++ labs/CompositionCollectionView/samples/CompositionCollectionView.Samples/MazeSample.xaml.cs
@@ -6,6 +6,7 @@ using CommunityToolkit.Labs.Core.SourceGenerators.Attributes;
 using CommunityToolkit.Labs.WinUI.CompositionCollectionView;
 using Microsoft.Toolkit.Uwp.UI.Animations.ExpressionsFork;
 using System.Numerics;
+using System.Xml.Linq;
 
 #if !WINAPPSDK
 using Windows.Foundation;
@@ -40,12 +41,12 @@ namespace CompositionCollectionView.Sample
     [ToolkitSample(id: nameof(MazeSample), "Maze layout", description: "Layout driven by an interaction tracker.")]
     public sealed partial class MazeSample : Page
     {
-        enum TileType { Floor, Ceiling, HorizontalWall, VerticalWall }
+        enum TileType { Floor, Ceiling, HorizontalWall, VerticalWall, Goal }
 
         const int TileWidth = 100;
         const int MazeSize = 10;
 
-        static readonly Vector3 GoalPosition = new Vector3(900, -900, 0);
+        static readonly Vector3 GoalPosition = new Vector3(9, 0, 0);
 
         private uint nextEntityId;
 
@@ -84,6 +85,11 @@ namespace CompositionCollectionView.Sample
                     }
                 }
             }
+
+            var maze = new Maze(MazeSize, MazeSize);
+            tiles.AddRange(maze.Walls);
+
+            tiles.Add(new(TileType.Goal, (int)GoalPosition.X, (int)GoalPosition.Y));
 
             elements = tiles.OrderBy(x => x.Y).Select(x => CreateElement(x)).ToList();
 
@@ -163,13 +169,13 @@ namespace CompositionCollectionView.Sample
 
                 var yPosition = tileType switch
                 {
+                    TileType.Goal => ((int)y! + 0.5f) * TileWidth,
                     _ => (int)y! * TileWidth
                 };
 
                 var height = tileType switch
                 {
                     TileType.Floor => TileWidth,
-                    TileType.Ceiling => 0,
                     _ => 0
                 };
 
@@ -199,6 +205,7 @@ namespace CompositionCollectionView.Sample
                     TileType.Ceiling => Quaternion.CreateFromYawPitchRoll(0, MathF.PI / 2, 0),
                     TileType.VerticalWall => Quaternion.CreateFromYawPitchRoll(-MathF.PI / 2, 0, 0),
                     TileType.HorizontalWall => Quaternion.Identity,
+                    TileType.Goal => Quaternion.Identity,
                     _ => Quaternion.Identity
                 };
 
@@ -255,7 +262,7 @@ namespace CompositionCollectionView.Sample
 
                 AnimatableNodes.GetOrCreateVector3Node(PositionNode, Vector3.Zero).Animate(ExpressionFunctions.Vector3(
                     ExpressionFunctions.Sin(rotationNode.Reference.Y) * mazeSide * 1.5f,
-                    mazeSide / 5,
+                    mazeSide / 3,
                     -ExpressionFunctions.Cos(rotationNode.Reference.Y) * mazeSide * 1.5f
                 ) + (Vector3Node)mazeCenter);
 
@@ -271,6 +278,16 @@ namespace CompositionCollectionView.Sample
             private void OnTapped(object sender, TappedRoutedEventArgs e)
             {
                 TransitionTo(x => new TraversableMazeLayout(this));
+            }
+
+            public override ScalarNode GetElementOpacityNode(ElementReference<uint> element)
+            {
+                element.Properties.TryGetValue(nameof(Tile.Type), out var tileType);
+                return tileType switch
+                {
+                    TileType.Ceiling => 0.3f,
+                    _ => 1
+                };
             }
         }
 
@@ -330,7 +347,7 @@ namespace CompositionCollectionView.Sample
 
             private void OnTrackerIdleStateEntered(InteractionTracker sender, InteractionTrackerIdleStateEnteredArgs args)
             {
-                if (Vector3.Distance(sender.Position, GoalPosition) < TileWidth)
+                if (Vector3.Distance(sender.Position, new Vector3(GoalPosition.X * TileWidth, -GoalPosition.Y * TileWidth, 0)) < TileWidth)
                 {
                     TransitionTo(x => new SpinningMazeLayout(this));
                 }
@@ -355,13 +372,32 @@ namespace CompositionCollectionView.Sample
                 var trackerBehavior = GetBehavior<InteractionTrackerBehavior<uint>>();
 
                 trackerBehavior.Tracker.MaxPosition = new Vector3(TileWidth * (MazeSize - 1), 0, 0);
-                trackerBehavior.Tracker.MinPosition = new Vector3(0, -TileWidth * (MazeSize - 1), 0);
+                trackerBehavior.Tracker.MinPosition = new Vector3(0, -TileWidth * (MazeSize - 1) + 20, 0);
+            }
+
+            public override ScalarNode GetElementOpacityNode(ElementReference<uint> element)
+            {
+                element.Properties.TryGetValue(nameof(Tile.X), out var x);
+                element.Properties.TryGetValue(nameof(Tile.Y), out var y);
+                element.Properties.TryGetValue(nameof(Tile.Type), out var tileType);
+
+                return (x, y, tileType) switch
+                {
+                    (_, _, TileType.Ceiling) => 1,
+                    (_, _, TileType.Floor) => 1,
+                    (_, _, TileType.Goal) => 1,
+                    (0, _, TileType.VerticalWall) => 1,
+                    (MazeSize, _, TileType.VerticalWall) => 1,
+                    (_, 0, TileType.HorizontalWall) => 1,
+                    (_, MazeSize, TileType.HorizontalWall) => 1,
+                    _ => 0.3f
+                };
             }
         }
 
         private class TileControl
         {
-            private static ImageBrush WallBrush, CeilingBrush, FloorBrush;
+            private static ImageBrush WallBrush, CeilingBrush, FloorBrush, GoalBrush;
 
             public static Rectangle Create() => new Rectangle()
             {
@@ -385,14 +421,69 @@ namespace CompositionCollectionView.Sample
                 {
                     ImageSource = new BitmapImage(new Uri("ms-appx:///CompositionCollectionViewExperiment.Samples/Assets/floor.bmp"))
                 };
+
+                GoalBrush = GoalBrush ?? new ImageBrush()
+                {
+                    ImageSource = new BitmapImage(new Uri("ms-appx:///CompositionCollectionViewExperiment.Samples/Assets/face.png"))
+                };
             }
 
             public static ImageBrush BrushFor(TileType type) => type switch
             {
                 TileType.Ceiling => CeilingBrush,
                 TileType.Floor => FloorBrush,
+                TileType.Goal => GoalBrush,
                 _ => WallBrush
             };
+        }
+
+        private class Maze
+        {
+            public IReadOnlyList<Tile> Walls { get; init; }
+
+            public Maze(int width, int height)
+            {
+                Queue<Rect> _pendingRooms = new();
+                _pendingRooms.Enqueue(new Rect(0, 0, width, height));
+
+                List<Tile> walls = new();
+                var r = new Random();
+
+                while (_pendingRooms.TryDequeue(out var room))
+                {
+                    var attemptHorizontalWall = r.NextDouble() > 0.5f;
+                    if (attemptHorizontalWall && room.Height > 1)
+                    {
+                        var wallY = 1 + Math.Floor(r.NextDouble() * room.Height);
+                        var doorX = Math.Floor(r.NextDouble() * (room.Width + 1));
+                        for (var x = 0; x < room.Width; x++)
+                        {
+                            if (x != doorX)
+                            {
+                                walls.Add(new(TileType.HorizontalWall, (int)(x + room.X), (int)(wallY + room.Y)));
+                            }
+                        }
+                        _pendingRooms.Enqueue(new Rect(room.X, room.Y, room.Width, wallY));
+                        _pendingRooms.Enqueue(new Rect(room.X, room.Y + wallY, room.Width, (room.Height - wallY)));
+                    }
+                    else if (room.Width > 1)
+                    {
+                        var wallX = 1 + Math.Floor(r.NextDouble() * room.Width);
+                        var doorY = Math.Floor(r.NextDouble() * (room.Height + 1));
+                        for (var y = 0; y < room.Height; y++)
+                        {
+                            if (y != doorY)
+                            {
+                                walls.Add(new(TileType.VerticalWall, (int)(wallX + room.X), (int)(y + room.Y)));
+                            }
+                        }
+                        _pendingRooms.Enqueue(new Rect(room.X, room.Y, wallX, room.Height));
+                        _pendingRooms.Enqueue(new Rect(room.X + wallX, room.Y, (room.Width - wallX), room.Height));
+                    }
+                }
+
+                Walls = walls;
+            }
         }
     }
 }
