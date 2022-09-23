dad5f5e3f9c9de40adc73b28fb8c5663e8c77124
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2022-09-23 13:05:48 -0700

    Clean-up platform to condense/consolidate code to make it easier to read/follow/maintain

--- labs/RivePlayer/src/RivePlayer.Platform.cs
+++ labs/RivePlayer/src/RivePlayer.Platform.cs
@@ -2,20 +2,16 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace CommunityToolkit.Labs.WinUI.Rive;
-
-// This file contains platform-specific customizations of RivePlayer.
-
-#if WINDOWS_WINAPPSDK || HAS_UNO_WASM
-
 #if WINDOWS_WINAPPSDK
 using SkiaSharp.Views.Windows;
 #else
+// SkiaSharp.Views.UWP is on Uno too.
 using SkiaSharp.Views.UWP;
 #endif
 
-// WinAppSdk doesn't have SKSwapChainPanel yet.
-// SKSwapChainPanel doesn't work in WASM yet.
+namespace CommunityToolkit.Labs.WinUI.Rive;
+
+// This file contains platform-specific customizations of RivePlayer.
 public partial class RivePlayer
 {
     // SKXamlCanvas doesn't support rendering in a background thread.
@@ -26,9 +22,18 @@ public partial class RivePlayer
         _skiaSurface = GetTemplateChild(SkiaSurfacePartName) as ContentPresenter;
         if (_skiaSurface != null && _skiaSurface.Content == null)
         {
+#if WINDOWS_WINAPPSDK || HAS_UNO_WASM
+            // WinAppSdk doesn't have SKSwapChainPanel yet.
+            // SKSwapChainPanel doesn't work in WASM yet.
             var xamlCanvas = new SKXamlCanvas();
             xamlCanvas.PaintSurface += OnPaintSurface;
             _skiaSurface.Content = xamlCanvas;
+#else
+            // SKSwapChainPanel performs better than SKXamlCanvas.
+            var swapChainPanel = new SKSwapChainPanel();
+            swapChainPanel.PaintSurface += OnPaintSurface;
+            _skiaSurface.Content = swapChainPanel;
+#endif
             _invalidateTimer = new InvalidateTimer(this, fps: 120);
         }
         base.OnApplyTemplate();
@@ -36,41 +41,21 @@ public partial class RivePlayer
 
     internal void Invalidate()
     {
+#if WINDOWS_WINAPPSDK || HAS_UNO_WASM
         var xamlCanvas = _skiaSurface?.Content as SKXamlCanvas;
         xamlCanvas?.Invalidate();
+#else
+        var swapChainPanel = _skiaSurface?.Content as SKSwapChainPanel;
+        swapChainPanel?.Invalidate();
+#endif
     }
 
+#if WINDOWS_WINAPPSDK || HAS_UNO_WASM
     private void OnPaintSurface(object? sender, SKPaintSurfaceEventArgs e)
     {
         this.PaintNextAnimationFrame(e.Surface, e.Info.Width, e.Info.Height);
     }
 #else
-
-// SkiaSharp.Views.UWP is on Uno too.
-using SkiaSharp.Views.UWP;
-
-// SKSwapChainPanel performs better than SKXamlCanvas.
-public partial class RivePlayer
-{
-    public bool DrawInBackground { get; set; }
-
-    protected override void OnApplyTemplate()
-    {
-        _skiaSurface = GetTemplateChild(SkiaSurfacePartName) as ContentPresenter;
-        if (_skiaSurface != null && _skiaSurface.Content == null)
-        {
-            var swapChainPanel = new SKSwapChainPanel();
-            swapChainPanel.PaintSurface += OnPaintSurface;
-            _skiaSurface.Content = swapChainPanel;
-            _invalidateTimer = new InvalidateTimer(this, fps: 120);
-        }
-        base.OnApplyTemplate();
-    }
-
-    internal void Invalidate()
-    {
-    }
-
     private void OnPaintSurface(object? sender, SKPaintGLSurfaceEventArgs e)
     {
         this.PaintNextAnimationFrame(e.Surface, e.BackendRenderTarget.Width, e.BackendRenderTarget.Height);
