13da2ec4be803a80b2a4ab0ed2514aee7c81e413
Chris Dalton <chris@rive.app>
2022-09-29 22:50:45 -0600

    Use an http URL in the WASM sample
    The ms-appx url doesn't seem to load in WASM.

--- labs/RivePlayer/samples/RivePlayer.Samples/RivePlayerCustomSample.xaml.cs
+++ labs/RivePlayer/samples/RivePlayer.Samples/RivePlayerCustomSample.xaml.cs
@@ -15,5 +15,9 @@ public sealed partial class RivePlayerCustomSample : Page
     public RivePlayerCustomSample()
     {
         this.InitializeComponent();
+#if HAS_UNO
+        // The Uno WASM platform seems to have trouble with our "ms-appx://" URI. Give it an http.
+        this.RivePlayer.Source = "https://public.rive.app/community/runtime-files/2244-4463-animated-login-screen.riv";
+#endif
     }
 }
