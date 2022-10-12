9b1650af3afcf57b1b159c8de7893806e4cf0f64
Chris Dalton <99840794+csmartdalton@users.noreply.github.com>
2022-10-11 22:45:35 -0600

    Update SkiaSharp package selection logic
    Co-authored-by: Arlo <arlo.godfrey@outlook.com>

--- labs/RivePlayer/src/RivePlayer.Platform.cs
+++ labs/RivePlayer/src/RivePlayer.Platform.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#if WINDOWS_UWP || (WINUI2 && HAS_UNO_WASM)
+#if !WINAPPSDK
  // WASM on WinUI2 needs SkiaSharp.Views.UWP too.
 using SkiaSharp.Views.UWP;
 #else
