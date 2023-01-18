49406e5645fdfdb88f02f558ebc34fa963160d3c
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2023-01-18 15:02:54 -0800

    Add helper to bat file for GenerateAllSolution to pass parameter to PowerShell script (if provided)

--- GenerateAllSolution.bat
+++ GenerateAllSolution.bat
@@ -1,1 +1,5 @@
-@powershell .\common\GenerateAllSolution.ps1
+@ECHO OFF
+SET "IncludeHeads=%1"
+IF "%IncludeHeads%"=="" SET "IncludeHeads=all"
+
+powershell .\common\GenerateAllSolution.ps1 -IncludeHeads %IncludeHeads%
