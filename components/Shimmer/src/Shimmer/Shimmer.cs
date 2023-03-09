650988c2b80a1698239084747ebaefcc3e500bc7
Niels Laute <niels.laute@live.nl>
2023-03-09 14:24:26 +0100

    Remove nullable warnings

--- components/Shimmer/src/Shimmer/Shimmer.cs
+++ components/Shimmer/src/Shimmer/Shimmer.cs
@@ -27,31 +27,27 @@ namespace CommunityToolkit.Labs.WinUI;
 /// A generic shimmer control that can be used to construct a beautiful loading effect.
 /// </summary>
 [TemplatePart(Name = PART_Shape, Type = typeof(Rectangle))]
-#pragma warning disable CA1001 // Types that own disposable fields should be disposable
 public partial class Shimmer : Control
-#pragma warning restore CA1001 // Types that own disposable fields should be disposable
 {
     private const float InitialStartPointX = -7.92f;
     private const string PART_Shape = "Shape";
 
-    private Vector2Node _sizeAnimation;
-    private Vector2KeyFrameAnimation _gradientStartPointAnimation;
-    private Vector2KeyFrameAnimation _gradientEndPointAnimation;
-    private CompositionColorGradientStop _gradientStop1;
-    private CompositionColorGradientStop _gradientStop2;
-    private CompositionColorGradientStop _gradientStop3;
-    private CompositionColorGradientStop _gradientStop4;
-    private CompositionRoundedRectangleGeometry _rectangleGeometry;
-    private ShapeVisual _shapeVisual;
-    private CompositionLinearGradientBrush _shimmerMaskGradient;
+    private Vector2Node? _sizeAnimation;
+    private Vector2KeyFrameAnimation? _gradientStartPointAnimation;
+    private Vector2KeyFrameAnimation? _gradientEndPointAnimation;
+    private CompositionColorGradientStop? _gradientStop1;
+    private CompositionColorGradientStop? _gradientStop2;
+    private CompositionColorGradientStop? _gradientStop3;
+    private CompositionColorGradientStop? _gradientStop4;
+    private CompositionRoundedRectangleGeometry? _rectangleGeometry;
+    private ShapeVisual? _shapeVisual;
+    private CompositionLinearGradientBrush? _shimmerMaskGradient;
     private Border? _shape;
 
     private bool _initialized;
     private bool _animationStarted;
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public Shimmer()
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     {
         DefaultStyleKey = typeof(Shimmer);
         Loaded += OnLoaded;
@@ -88,13 +84,14 @@ public partial class Shimmer : Control
         {
             ElementCompositionPreview.SetElementChildVisual(_shape, null);
 
-            _rectangleGeometry.Dispose();
-            _shapeVisual.Dispose();
-            _shimmerMaskGradient.Dispose();
-            _gradientStop1.Dispose();
-            _gradientStop2.Dispose();
-            _gradientStop3.Dispose();
-            _gradientStop4.Dispose();
+            
+            _rectangleGeometry!.Dispose();
+            _shapeVisual!.Dispose();
+            _shimmerMaskGradient!.Dispose();
+            _gradientStop1!.Dispose();
+            _gradientStop2!.Dispose();
+            _gradientStop3!.Dispose();
+            _gradientStop4!.Dispose();
 
             _initialized = false;
         }
@@ -145,13 +142,13 @@ public partial class Shimmer : Control
 
     private void SetGradientAndStops()
     {
-        _shimmerMaskGradient.StartPoint = new Vector2(InitialStartPointX, 0.0f);
+        _shimmerMaskGradient!.StartPoint = new Vector2(InitialStartPointX, 0.0f);
         _shimmerMaskGradient.EndPoint = new Vector2(0.0f, 1.0f); //Vector2.One
 
-        _gradientStop1.Offset = 0.273f;
-        _gradientStop2.Offset = 0.436f;
-        _gradientStop3.Offset = 0.482f;
-        _gradientStop4.Offset = 0.643f;
+        _gradientStop1!.Offset = 0.273f;
+        _gradientStop2!.Offset = 0.436f;
+        _gradientStop3!.Offset = 0.482f;
+        _gradientStop4!.Offset = 0.643f;
 
         _shimmerMaskGradient.ColorStops.Add(_gradientStop1);
         _shimmerMaskGradient.ColorStops.Add(_gradientStop2);
@@ -165,16 +162,16 @@ public partial class Shimmer : Control
         {
             case ElementTheme.Default:
             case ElementTheme.Dark:
-                _gradientStop1.Color = Color.FromArgb((byte)(255 * 3.26 / 100), 255, 255, 255);
-                _gradientStop2.Color = Color.FromArgb((byte)(255 * 6.05 / 100), 255, 255, 255);
-                _gradientStop3.Color = Color.FromArgb((byte)(255 * 6.05 / 100), 255, 255, 255);
-                _gradientStop4.Color = Color.FromArgb((byte)(255 * 3.26 / 100), 255, 255, 255);
+                _gradientStop1!.Color = Color.FromArgb((byte)(255 * 3.26 / 100), 255, 255, 255);
+                _gradientStop2!.Color = Color.FromArgb((byte)(255 * 6.05 / 100), 255, 255, 255);
+                _gradientStop3!.Color = Color.FromArgb((byte)(255 * 6.05 / 100), 255, 255, 255);
+                _gradientStop4!.Color = Color.FromArgb((byte)(255 * 3.26 / 100), 255, 255, 255);
                 break;
             case ElementTheme.Light:
-                _gradientStop1.Color = Color.FromArgb((byte)(255 * 5.37 / 100), 0, 0, 0);
-                _gradientStop2.Color = Color.FromArgb((byte)(255 * 2.89 / 100), 0, 0, 0);
-                _gradientStop3.Color = Color.FromArgb((byte)(255 * 2.89 / 100), 0, 0, 0);
-                _gradientStop4.Color = Color.FromArgb((byte)(255 * 5.37 / 100), 0, 0, 0);
+                _gradientStop1!.Color = Color.FromArgb((byte)(255 * 5.37 / 100), 0, 0, 0);
+                _gradientStop2!.Color = Color.FromArgb((byte)(255 * 2.89 / 100), 0, 0, 0);
+                _gradientStop3!.Color = Color.FromArgb((byte)(255 * 2.89 / 100), 0, 0, 0);
+                _gradientStop4!.Color = Color.FromArgb((byte)(255 * 5.37 / 100), 0, 0, 0);
                 break;
         }
     }
@@ -196,7 +193,7 @@ public partial class Shimmer : Control
         _gradientStartPointAnimation.IterationBehavior = AnimationIterationBehavior.Forever;
         _gradientStartPointAnimation.InsertKeyFrame(0.0f, new Vector2(InitialStartPointX, 0.0f));
         _gradientStartPointAnimation.InsertKeyFrame(1.0f, Vector2.Zero);
-        _shimmerMaskGradient.StartAnimation(nameof(CompositionLinearGradientBrush.StartPoint), _gradientStartPointAnimation);
+        _shimmerMaskGradient!.StartAnimation(nameof(CompositionLinearGradientBrush.StartPoint), _gradientStartPointAnimation);
 
         _gradientEndPointAnimation = rootVisual.Compositor.CreateVector2KeyFrameAnimation();
         _gradientEndPointAnimation.Duration = Duration;
@@ -215,14 +212,14 @@ public partial class Shimmer : Control
             return;
         }
 
-        _shapeVisual.StopAnimation(nameof(ShapeVisual.Size));
-        _rectangleGeometry.StopAnimation(nameof(CompositionRoundedRectangleGeometry.Size));
-        _shimmerMaskGradient.StopAnimation(nameof(CompositionLinearGradientBrush.StartPoint));
+        _shapeVisual!.StopAnimation(nameof(ShapeVisual.Size));
+        _rectangleGeometry!.StopAnimation(nameof(CompositionRoundedRectangleGeometry.Size));
+        _shimmerMaskGradient!.StopAnimation(nameof(CompositionLinearGradientBrush.StartPoint));
         _shimmerMaskGradient.StopAnimation(nameof(CompositionLinearGradientBrush.EndPoint));
 
-        _sizeAnimation.Dispose();
-        _gradientStartPointAnimation.Dispose();
-        _gradientEndPointAnimation.Dispose();
+        _sizeAnimation!.Dispose();
+        _gradientStartPointAnimation!.Dispose();
+        _gradientEndPointAnimation!.Dispose();
         _animationStarted = false;
     }
 }
