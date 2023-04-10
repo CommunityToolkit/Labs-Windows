d4664bce52ea7fb796c418b2d814c24d04d2f1fe
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2023-04-10 10:48:16 -0700

    Implement intial test implementation of DataTable coordinated panel
    Coordinates with a standard Grid in the Items control header.

--- components/DataTable/src/DataTable.cs
+++ components/DataTable/src/DataTable.cs
@@ -4,105 +4,74 @@
 
 namespace CommunityToolkit.WinUI.Controls;
 
-/// <summary>
-/// This is an example control based off of the BoxPanel sample here: https://docs.microsoft.com/windows/apps/design/layout/boxpanel-example-custom-panel. If you need this similar sort of layout component for an application, see UniformGrid in the Toolkit.
-/// It is provided as an example of how to inherit from another control like <see cref="Panel"/>.
-/// You can choose to start here or from the <see cref="DataTable_ClassicBinding"/> or <see cref="DataTable_xBind"/> example components. Remove unused components and rename as appropriate.
-/// </summary>
 public partial class DataTable : Panel
 {
-    /// <summary>
-    /// Identifies the <see cref="Orientation"/> property.
-    /// </summary>
-    public static readonly DependencyProperty OrientationProperty =
-        DependencyProperty.Register(nameof(Orientation), typeof(Orientation), typeof(DataTable), new PropertyMetadata(null, OnOrientationChanged));
+    // TODO: Create our own helper class here for the Header as well vs. straight-Grid.
+    // TODO: WeakReference?
+    private Grid? _parentGrid;
 
-    /// <summary>
-    /// Gets the preference of the rows/columns when there are a non-square number of children. Defaults to Vertical.
-    /// </summary>
-    public Orientation Orientation
+    private Grid? FindParentHeader()
     {
-        get { return (Orientation)GetValue(OrientationProperty); }
-        set { SetValue(OrientationProperty, value); }
-    }
+        // TODO: Think about this expression instead...
+        //       Drawback: Can't have Grid between table and header
+        //       Positive: don't have to restart climbing the Visual Tree if we don't find ItemsPresenter...
+        ////var parent = this.FindAscendant<FrameworkElement>(static (element) => element is ItemsPresenter or Grid);
 
-    // Invalidate our layout when the property changes.
-    private static void OnOrientationChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
-    {
-        if (dependencyObject is DataTable panel)
+        // 1a. Get parent ItemsPresenter to find header
+        if (this.FindAscendant<ItemsPresenter>() is ItemsPresenter itemsPresenter)
         {
-            panel.InvalidateMeasure();
-        }
-    }
+            // 2. Quickly check if the header is just what we're looking for.
+            if (itemsPresenter.Header is Grid grid)
+            {
+                return grid;
+            }
 
-    // Store calculations we want to use between the Measure and Arrange methods.
-    int _columnCount;
-    double _cellWidth, _cellHeight;
+            // 3. Otherwise, try and find the inner thing we want.
+            return itemsPresenter.FindDescendant<Grid>();
+        }
+
+        // 1b. If we can't find the ItemsPresenter, then we reach up outside to find the next thing we could use as a parent
+        return this.FindAscendant<Grid>();
+    }
 
     protected override Size MeasureOverride(Size availableSize)
     {
-        // Determine the square that can contain this number of items.
-        var maxrc = (int)Math.Ceiling(Math.Sqrt(Children.Count));
-        // Get an aspect ratio from availableSize, decides whether to trim row or column.
-        var aspectratio = availableSize.Width / availableSize.Height;
-        if (Orientation == Orientation.Vertical) { aspectratio = 1 / aspectratio; }
+        // We should probably only have to do this once ever?
+        _parentGrid ??= FindParentHeader();
 
-        int rowcount;
-
-        // Now trim this square down to a rect, many times an entire row or column can be omitted.
-        if (aspectratio > 1)
+        // If we don't have a grid, just measure first child to get row height
+        if (_parentGrid is null && Children.Count > 0)
         {
-            rowcount = maxrc;
-            _columnCount = (maxrc > 2 && Children.Count <= maxrc * (maxrc - 1)) ? maxrc - 1 : maxrc;
-        }
-        else
-        {
-            rowcount = (maxrc > 2 && Children.Count <= maxrc * (maxrc - 1)) ? maxrc - 1 : maxrc;
-            _columnCount = maxrc;
+            Children[0].Measure(availableSize);
+            return new Size(availableSize.Width, Children[0].DesiredSize.Height);
         }
 
-        // Now that we have a column count, divide available horizontal, that's our cell width.
-        _cellWidth = (int)Math.Floor(availableSize.Width / _columnCount);
-        // Next get a cell height, same logic of dividing available vertical by rowcount.
-        _cellHeight = Double.IsInfinity(availableSize.Height) ? Double.PositiveInfinity : availableSize.Height / rowcount;
-
-        double maxcellheight = 0;
-
-        foreach (UIElement child in Children)
-        {
-            child.Measure(new Size(_cellWidth, _cellHeight));
-            maxcellheight = (child.DesiredSize.Height > maxcellheight) ? child.DesiredSize.Height : maxcellheight;
-        }
-
-        return LimitUnboundedSize(availableSize, maxcellheight);
-    }
-
-    // This method limits the panel height when no limit is imposed by the panel's parent.
-    // That can happen to height if the panel is close to the root of main app window.
-    // In this case, base the height of a cell on the max height from desired size
-    // and base the height of the panel on that number times the #rows.
-    Size LimitUnboundedSize(Size input, double maxcellheight)
-    { 
-        if (Double.IsInfinity(input.Height))
-        {
-            input.Height = maxcellheight * _columnCount;
-            _cellHeight = maxcellheight;
-        }
-        return input;
+        // Otherwise, return our parent's size as the desired size.
+        return _parentGrid?.DesiredSize ?? new Size(availableSize.Width, 0);
     }
 
     protected override Size ArrangeOverride(Size finalSize)
     {
-        int count = 1;
-        double x, y;
-        foreach (UIElement child in Children)
+        int column = 0;
+        double x = 0;
+
+        if (_parentGrid != null)
         {
-            x = (count - 1) % _columnCount * _cellWidth;
-            y = ((int)(count - 1) / _columnCount) * _cellHeight;
-            Point anchorPoint = new Point(x, y);
-            child.Arrange(new Rect(anchorPoint, child.DesiredSize));
-            count++;
+            foreach (UIElement child in Children)
+            {
+                if (child.Visibility == Visibility.Visible &&
+                    column < _parentGrid.ColumnDefinitions.Count)
+                {
+                    var width = _parentGrid.ColumnDefinitions[column++].ActualWidth;
+                    child.Arrange(new Rect(x, 0, width, finalSize.Height));
+
+                    x += width + _parentGrid.ColumnSpacing;
+                }
+            }
+
+            return new Size(x - _parentGrid.ColumnSpacing, finalSize.Height);
         }
+
         return finalSize;
     }
 }
