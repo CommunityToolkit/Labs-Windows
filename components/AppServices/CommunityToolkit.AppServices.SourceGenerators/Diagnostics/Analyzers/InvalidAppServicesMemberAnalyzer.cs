0375d1e9e20fc87e832c69ef1aac2674c5722453
Sergio Pedri <sergio0694@live.com>
2023-05-31 22:03:36 +0200

    Resolve symbol in compilation action in analyzer

--- components/AppServices/CommunityToolkit.AppServices.SourceGenerators/Diagnostics/Analyzers/InvalidAppServicesMemberAnalyzer.cs
+++ components/AppServices/CommunityToolkit.AppServices.SourceGenerators/Diagnostics/Analyzers/InvalidAppServicesMemberAnalyzer.cs
@@ -32,89 +32,96 @@ public sealed class InvalidAppServicesMemberAnalyzer : DiagnosticAnalyzer
         context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
         context.EnableConcurrentExecution();
 
-        // Register a callback for all named type symbols (ie. user defined types)
-        context.RegisterSymbolAction(static context =>
+        context.RegisterCompilationStartAction(static context =>
         {
-            // The symbol must be an interface
-            if (context.Symbol is not INamedTypeSymbol { TypeKind: TypeKind.Interface } interfaceSymbol)
+            // Get the symbol for [AppService]
+            if (context.Compilation.GetTypeByMetadataName("CommunityToolkit.AppServices.AppServiceAttribute") is not INamedTypeSymbol appServicesAttributeSymbol)
             {
                 return;
             }
 
-            INamedTypeSymbol appServicesAttributeSymbol = context.Compilation.GetTypeByMetadataName("CommunityToolkit.AppServices.AppServiceAttribute")!;
-
-            // Check whether the interface is an app services interface
-            if (!interfaceSymbol.HasOrInheritsAttribute(appServicesAttributeSymbol))
+            // Register a callback for all named type symbols (ie. user defined types)
+            context.RegisterSymbolAction(context =>
             {
-                return;
-            }
-
-            // Go through all interface members to analyze them. Here we need to go through all members, not just the ones immediately
-            // declared, as it's possible an interface with an invalid member will be inherited by another one that adds [AppServices].
-            // In that case, the base interface will not be analyzed (as it doesn't have [AppServices]), so the derived one will need
-            // to also go through inherited members to ensure that all members that the generator will process will actually be valid.
-            foreach (ISymbol memberSymbol in interfaceSymbol.GetAllMembers())
-            {
-                // If a method is not abstract nor virtual (ie. a DIM or static non-virtual interface member), it can just be ignored.
-                // The generated service type will not have to consider it as far as registering endpoints and generating members goes.
-                if (memberSymbol.IsIgnoredAppServicesMember())
+                // The symbol must be an interface
+                if (context.Symbol is not INamedTypeSymbol { TypeKind: TypeKind.Interface } interfaceSymbol)
                 {
-                    continue;
+                    return;
                 }
 
-                // All remaining members must be non-generic instance methods, which the generator will emit
-                if (memberSymbol is not IMethodSymbol { IsStatic: false, IsGenericMethod: false, ReturnType: INamedTypeSymbol returnTypeSymbol } methodSymbol)
+                // Check whether the interface is an app services interface
+                if (!interfaceSymbol.HasOrInheritsAttribute(appServicesAttributeSymbol))
                 {
-                    context.ReportDiagnostic(Diagnostic.Create(InvalidAppServicesMemberType, memberSymbol.Locations.FirstOrDefault(), memberSymbol, interfaceSymbol));
-
-                    continue;
+                    return;
                 }
 
-                // Validate the return type for the current method
-                if (!methodSymbol.TryGetParameterOrReturnType(out ParameterOrReturnType returnType) ||
-                    !returnType.IsValidReturnType())
+                // Go through all interface members to analyze them. Here we need to go through all members, not just the ones immediately
+                // declared, as it's possible an interface with an invalid member will be inherited by another one that adds [AppServices].
+                // In that case, the base interface will not be analyzed (as it doesn't have [AppServices]), so the derived one will need
+                // to also go through inherited members to ensure that all members that the generator will process will actually be valid.
+                foreach (ISymbol memberSymbol in interfaceSymbol.GetAllMembers())
                 {
-                    context.ReportDiagnostic(Diagnostic.Create(InvalidAppServicesMethodReturnType, memberSymbol.Locations.FirstOrDefault(), methodSymbol, interfaceSymbol, returnTypeSymbol));
-                }
-
-                int progressParametersCount = 0;
-                int cancellationTokenParametersCount = 0;
-
-                // Validate the method parameters
-                foreach (IParameterSymbol parameter in methodSymbol.Parameters)
-                {
-                    // First validate types that could possibly be allowed at all (ie. valid types)
-                    if (!parameter.TryGetParameterOrReturnType(out ParameterOrReturnType parameterType) ||
-                        !parameterType.IsValidParameterType())
+                    // If a method is not abstract nor virtual (ie. a DIM or static non-virtual interface member), it can just be ignored.
+                    // The generated service type will not have to consider it as far as registering endpoints and generating members goes.
+                    if (memberSymbol.IsIgnoredAppServicesMember())
                     {
-                        context.ReportDiagnostic(Diagnostic.Create(InvalidAppServicesMethodParameterType, parameter.Locations.FirstOrDefault(), parameter.Name, methodSymbol, interfaceSymbol, parameter.Type));
+                        continue;
+                    }
+
+                    // All remaining members must be non-generic instance methods, which the generator will emit
+                    if (memberSymbol is not IMethodSymbol { IsStatic: false, IsGenericMethod: false, ReturnType: INamedTypeSymbol returnTypeSymbol } methodSymbol)
+                    {
+                        context.ReportDiagnostic(Diagnostic.Create(InvalidAppServicesMemberType, memberSymbol.Locations.FirstOrDefault(), memberSymbol, interfaceSymbol));
 
                         continue;
                     }
 
-                    // Then check that the type is not an IProgress<T>, if one has already been discovered
-                    if (parameterType.HasFlag(ParameterOrReturnType.IProgressOfT))
+                    // Validate the return type for the current method
+                    if (!methodSymbol.TryGetParameterOrReturnType(out ParameterOrReturnType returnType) ||
+                        !returnType.IsValidReturnType())
                     {
-                        progressParametersCount++;
-
-                        if (progressParametersCount > 1)
-                        {
-                            context.ReportDiagnostic(Diagnostic.Create(InvalidRepeatedAppServicesMethodIProgressParameter, parameter.Locations.FirstOrDefault(), parameter.Name, methodSymbol, interfaceSymbol, parameter.Type));
-                        }
+                        context.ReportDiagnostic(Diagnostic.Create(InvalidAppServicesMethodReturnType, memberSymbol.Locations.FirstOrDefault(), methodSymbol, interfaceSymbol, returnTypeSymbol));
                     }
 
-                    // Lastly, check that the type is not a CancellationToken, if one has already been discovered
-                    if (parameterType.HasFlag(ParameterOrReturnType.CancellationToken))
-                    {
-                        cancellationTokenParametersCount++;
+                    int progressParametersCount = 0;
+                    int cancellationTokenParametersCount = 0;
 
-                        if (cancellationTokenParametersCount > 1)
+                    // Validate the method parameters
+                    foreach (IParameterSymbol parameter in methodSymbol.Parameters)
+                    {
+                        // First validate types that could possibly be allowed at all (ie. valid types)
+                        if (!parameter.TryGetParameterOrReturnType(out ParameterOrReturnType parameterType) ||
+                            !parameterType.IsValidParameterType())
                         {
-                            context.ReportDiagnostic(Diagnostic.Create(InvalidRepeatedAppServicesMethodCancellationTokenParameter, parameter.Locations.FirstOrDefault(), parameter.Name, methodSymbol, interfaceSymbol, parameter.Type));
+                            context.ReportDiagnostic(Diagnostic.Create(InvalidAppServicesMethodParameterType, parameter.Locations.FirstOrDefault(), parameter.Name, methodSymbol, interfaceSymbol, parameter.Type));
+
+                            continue;
+                        }
+
+                        // Then check that the type is not an IProgress<T>, if one has already been discovered
+                        if (parameterType.HasFlag(ParameterOrReturnType.IProgressOfT))
+                        {
+                            progressParametersCount++;
+
+                            if (progressParametersCount > 1)
+                            {
+                                context.ReportDiagnostic(Diagnostic.Create(InvalidRepeatedAppServicesMethodIProgressParameter, parameter.Locations.FirstOrDefault(), parameter.Name, methodSymbol, interfaceSymbol, parameter.Type));
+                            }
+                        }
+
+                        // Lastly, check that the type is not a CancellationToken, if one has already been discovered
+                        if (parameterType.HasFlag(ParameterOrReturnType.CancellationToken))
+                        {
+                            cancellationTokenParametersCount++;
+
+                            if (cancellationTokenParametersCount > 1)
+                            {
+                                context.ReportDiagnostic(Diagnostic.Create(InvalidRepeatedAppServicesMethodCancellationTokenParameter, parameter.Locations.FirstOrDefault(), parameter.Name, methodSymbol, interfaceSymbol, parameter.Type));
+                            }
                         }
                     }
                 }
-            }
-        }, SymbolKind.NamedType);
+            }, SymbolKind.NamedType);
+        });
     }
 }
