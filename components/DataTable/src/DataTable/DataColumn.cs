61549a45a05d0bbc3630e65729f5543ef97c917e
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2023-06-23 15:23:31 -0700

    Add ability to resize columns and them to react in realtime

--- components/DataTable/src/DataTable/DataColumn.cs
+++ components/DataTable/src/DataTable/DataColumn.cs
@@ -19,6 +19,11 @@ public partial class DataColumn : ContentControl
     internal double MaxChildDesiredWidth { get; set; }
 
     /// <summary>
+    /// Gets or sets the internal copy of the <see cref="DesiredWidth"/> property to be used in calculations, this gets manipulated in Auto-Size mode.
+    /// </summary>
+    internal GridLength CurrentWidth { get; private set; }
+
+    /// <summary>
     /// Gets or sets whether the column can be resized by the user.
     /// </summary>
     public bool CanResize
@@ -46,7 +51,16 @@ public partial class DataColumn : ContentControl
     /// Identifies the <see cref="DesiredWidth"/> property.
     /// </summary>
     public static readonly DependencyProperty DesiredWidthProperty =
-        DependencyProperty.Register(nameof(DesiredWidth), typeof(GridLength), typeof(DataColumn), new PropertyMetadata(GridLength.Auto));
+        DependencyProperty.Register(nameof(DesiredWidth), typeof(GridLength), typeof(DataColumn), new PropertyMetadata(GridLength.Auto, DesiredWidth_PropertyChanged));
+
+    private static void DesiredWidth_PropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        // If the developer updates the size of the column, update our internal copy
+        if (d is DataColumn col)
+        {
+            col.CurrentWidth = col.DesiredWidth;
+        }
+    }
 
     public DataColumn()
     {
@@ -58,6 +72,7 @@ public partial class DataColumn : ContentControl
         if (PART_ColumnSizer != null)
         {
             PART_ColumnSizer.TargetControl = null;
+            PART_ColumnSizer.ManipulationDelta -= this.PART_ColumnSizer_ManipulationDelta;
             PART_ColumnSizer.ManipulationCompleted -= this.PART_ColumnSizer_ManipulationCompleted;
         }
 
@@ -66,6 +81,7 @@ public partial class DataColumn : ContentControl
         if (PART_ColumnSizer != null)
         {
             PART_ColumnSizer.TargetControl = this;
+            PART_ColumnSizer.ManipulationDelta += this.PART_ColumnSizer_ManipulationDelta;
             PART_ColumnSizer.ManipulationCompleted += this.PART_ColumnSizer_ManipulationCompleted;
         }
 
@@ -79,8 +95,22 @@ public partial class DataColumn : ContentControl
         base.OnApplyTemplate();
     }
 
+    private void PART_ColumnSizer_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+    {
+        ResizeColumn();
+    }
+
     private void PART_ColumnSizer_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
     {
+        ResizeColumn();
+    }
+
+    private void ResizeColumn()
+    {
+        // Update our internal representation to be our size now as a fixed value.
+        CurrentWidth = new(this.ActualWidth);
+
+        // Notify the rest of the table to update
         if (_parent?.TryGetTarget(out DataTable? parent) == true
             && parent != null)
         {
