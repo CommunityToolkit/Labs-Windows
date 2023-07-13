cc1f4692a117e5285f6b61a9a50189dbad166fcb
Niels Laute <niels.laute@live.nl>
2023-07-13 11:22:56 +0200

    Adding file heads and missing namespaces for UWP

--- components/MarkdigUI/src/HtmlWriter.cs
+++ components/MarkdigUI/src/HtmlWriter.cs
@@ -1,3 +1,7 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using HtmlAgilityPack;
 using CommunityToolkit.Labs.WinUI.MarkdigUI.Renderers;
 using CommunityToolkit.Labs.WinUI.MarkdigUI.TextElements.Html;
--- components/MarkdigUI/src/TextElements/MyEmphasisInline.cs
+++ components/MarkdigUI/src/TextElements/MyEmphasisInline.cs
@@ -1,4 +1,9 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using Markdig.Syntax.Inlines;
+using Windows.UI.Text;
 
 namespace CommunityToolkit.Labs.WinUI.MarkdigUI.TextElements;
 
