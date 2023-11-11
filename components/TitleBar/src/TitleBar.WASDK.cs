27f440a4d595000b2f21b42ba276c5a272cbfbfe
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-11-11 15:32:22 +0330

    Remove unused Codes

--- components/TitleBar/src/TitleBar.WASDK.cs
+++ components/TitleBar/src/TitleBar.WASDK.cs
@@ -10,29 +10,11 @@ using Microsoft.UI.Xaml.Media;
 
 namespace CommunityToolkit.WinUI.Controls;
 
-[TemplatePart(Name = nameof(PART_ButtonsHolderColumn), Type = typeof(ColumnDefinition))]
-[TemplatePart(Name = nameof(PART_IconColumn), Type = typeof(ColumnDefinition))]
-[TemplatePart(Name = nameof(PART_TitleColumn), Type = typeof(ColumnDefinition))]
-[TemplatePart(Name = nameof(PART_LeftDragColumn), Type = typeof(ColumnDefinition))]
-[TemplatePart(Name = nameof(PART_ContentColumn), Type = typeof(ColumnDefinition))]
-[TemplatePart(Name = nameof(PART_FooterColumn), Type = typeof(ColumnDefinition))]
-[TemplatePart(Name = nameof(PART_RightDragColumn), Type = typeof(ColumnDefinition))]
-[TemplatePart(Name = nameof(PART_TitleHolder), Type = typeof(StackPanel))]
-[TemplatePart(Name = nameof(PART_RootGrid), Type = typeof(Grid))]
 [TemplatePart(Name = nameof(PART_FooterPresenter), Type = typeof(ContentPresenter))]
 [TemplatePart(Name = nameof(PART_ContentPresenter), Type = typeof(ContentPresenter))]
 
 public partial class TitleBar : Control
 {
-    ColumnDefinition? PART_ButtonsHolderColumn;
-    ColumnDefinition? PART_IconColumn;
-    ColumnDefinition? PART_TitleColumn;
-    ColumnDefinition? PART_LeftDragColumn;
-    ColumnDefinition? PART_ContentColumn;
-    ColumnDefinition? PART_FooterColumn;
-    ColumnDefinition? PART_RightDragColumn;
-    StackPanel? PART_TitleHolder;
-    Grid? PART_RootGrid;
     ContentPresenter? PART_ContentPresenter;
     ContentPresenter? PART_FooterPresenter;
 
@@ -61,16 +43,6 @@ public partial class TitleBar : Control
                 };
             }
 
-            // Set the width of padding columns in the UI.
-            PART_ButtonsHolderColumn = GetTemplateChild(nameof(PART_ButtonsHolderColumn)) as ColumnDefinition;
-            PART_IconColumn = GetTemplateChild(nameof(PART_IconColumn)) as ColumnDefinition;
-            PART_TitleColumn = GetTemplateChild(nameof(PART_TitleColumn)) as ColumnDefinition;
-            PART_LeftDragColumn = GetTemplateChild(nameof(PART_LeftDragColumn)) as ColumnDefinition;
-            PART_ContentColumn = GetTemplateChild(nameof(PART_ContentColumn)) as ColumnDefinition;
-            PART_RightDragColumn = GetTemplateChild(nameof(PART_RightDragColumn)) as ColumnDefinition;
-            PART_FooterColumn = GetTemplateChild(nameof(PART_FooterColumn)) as ColumnDefinition;
-            PART_TitleHolder = GetTemplateChild(nameof(PART_TitleHolder)) as StackPanel;
-            PART_RootGrid = GetTemplateChild(nameof(PART_RootGrid)) as Grid;
             PART_ContentPresenter = GetTemplateChild(nameof(PART_ContentPresenter)) as ContentPresenter;
             PART_FooterPresenter = GetTemplateChild(nameof(PART_FooterPresenter)) as ContentPresenter;
 
