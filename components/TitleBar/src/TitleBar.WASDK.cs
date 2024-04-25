396c61a8e02c81eb6554750f9ff952315387e61c
Arlo Godfrey <arlo.godfrey@outlook.com>
2024-04-25 15:48:29 -0500

    Restrict TitleBar MultiTargets to uwp and wasdk

--- components/TitleBar/src/TitleBar.WASDK.cs
+++ components/TitleBar/src/TitleBar.WASDK.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#if WINAPPSDK
+#if WINDOWS_WINAPPSDK && !HAS_UNO
 using Windows.Graphics;
 using Microsoft.UI;
 using Microsoft.UI.Input;
