8e116adc1f9db2fb7d133ff9c2f5603588f63b79
Vincent Gromfeld <vigromfe@microsoft.com>
2024-05-21 17:12:25 +0200

    Add ribbon control

---
+++ components/Ribbon/samples/Ribbon.md
@@ -0,0 +1,74 @@
+---
+title: Ribbon
+author: vgromfeld
+description: An office like ribbon.
+keywords: Ribbon, Control
+dev_langs:
+  - csharp
+category: Controls
+subcategory: Layout
+experimental: true
+discussion-id: 0
+issue-id: 0
+icon: Assets/Ribbon.png
+---
+
+<!-- Be sure to update the discussion/issue numbers above with your Labs discussion/issue id numbers in order for UI links to them from the sample app to work. -->
+
+# Ribbon
+
+An Office like Ribbon control which displays groups of commands. If there is not enough space to display all the groups,
+some of them can be collapsed based on a priority order.
+
+> [!Sample RibbonCustomSample]
+
+## RibbonGroup
+
+A basic group displayed in a Ribbon.
+It mostly adds a *label* to some content and will not collapse if there is not enough space available.
+
+### Label
+The label of the group.
+
+### Content
+The content of the group.
+
+## RibbonCollapsibleGroup
+
+A `RibbonGroup` which can be collapsed if its content does not fit in the available Ribbon's space.
+When collapsed, the group is displayed as a single icon button. Clicking on this button opens
+a flyout containing the group's content.
+
+### IconSource
+The icon to display when the group is collapsed.
+
+### AutoCloseflyout
+Set to true to automatically close the overflow flyout if one interactive element is clicked.
+Note that the logic to detect the click is very basic. It will request the flyout to close
+for all the handled pointer released events. It assumes that if the pointer has been handled
+something reacted to it. It works well for buttons or check boxes but does not work for text
+or combo boxes.
+
+### Priority
+The priority of the group.
+The group with the lower priority will be the first one to be collapsed.
+
+### CollapsedAccessKey
+The access key to access the collapsed button and open the flyout when the group is collapsed.
+
+### RequestedWidths
+
+The list of requested widths for the group.
+If null or empty, the group will automatically use the size of its content.
+If set, the group will use the smallest provided width fitting in the ribbon.
+This is useful if the group contains a variable size control which can adjust
+its width (like a GridView with several items).
+
+### State
+The state of the group (collapsed or visible). This property is used by the `RibbonPanel`.
+
+## RibbonPanel
+
+The inner panel of the Ribbon control. It displays the groups inside the `Ribbon` and
+automatically collapse the `CollapsibleGroup` elements based on their priority order if
+there is not enough space available.
---
+++ components/Ribbon/src/Ribbon.cs
@@ -0,0 +1,201 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Specialized;
+
+namespace CommunityToolkit.WinUI.Controls;
+
+/// <summary>
+/// An Office like Ribbon control which displays groups of commands. If there is not enough space to display all the groups,
+/// some of them can be collapsed based on a priority order.
+/// </summary>
+[ContentProperty(Name = nameof(Items))]
+[TemplatePart(Name = PanelTemplatePart, Type = typeof(Panel))]
+[TemplatePart(Name = ScrollViewerTemplatePart, Type = typeof(ScrollViewer))]
+[TemplatePart(Name = ScrollDecrementButtonTempatePart, Type = typeof(ButtonBase))]
+[TemplatePart(Name = ScrollIncrementButtonTempatePart, Type = typeof(ButtonBase))]
+[TemplateVisualState(GroupName = ScrollButtonGroupNameTemplatePart, Name = NoButtonsStateTemplatePart)]
+[TemplateVisualState(GroupName = ScrollButtonGroupNameTemplatePart, Name = DecrementButtonStateTemplatePart)]
+[TemplateVisualState(GroupName = ScrollButtonGroupNameTemplatePart, Name = IncrementButtonStateTemplatePart)]
+[TemplateVisualState(GroupName = ScrollButtonGroupNameTemplatePart, Name = BothButtonsStateTemplatePart)]
+public sealed class Ribbon : Control
+{
+    private const string PanelTemplatePart = "Panel";
+    private const string ScrollViewerTemplatePart = "ScrollViewer";
+    private const string ScrollDecrementButtonTempatePart = "ScrollDecrementButton";
+    private const string ScrollIncrementButtonTempatePart = "ScrollIncrementButton";
+    private const string ScrollButtonGroupNameTemplatePart = "ScrollButtonGroup";
+    private const string NoButtonsStateTemplatePart = "NoButtons";
+    private const string DecrementButtonStateTemplatePart = "DecrementButton";
+    private const string IncrementButtonStateTemplatePart = "IncrementButton";
+    private const string BothButtonsStateTemplatePart = "BothButtons";
+
+    private Panel? _panel;
+    private ScrollViewer? _scrollViewer;
+    private ButtonBase? _decrementButton;
+    private ButtonBase? _incrementButton;
+    private readonly ObservableCollection<UIElement> _items;
+
+    /// <summary>
+    /// The DP to store the <see cref="ScrollStep"/> property value.
+    /// </summary>
+    public static readonly DependencyProperty ScrollStepProperty = DependencyProperty.Register(
+        nameof(ScrollStep),
+        typeof(double),
+        typeof(Ribbon),
+        new PropertyMetadata(20.0));
+
+    /// <summary>
+    /// The amount to add or remove from the current scroll position.
+    /// </summary>
+    public double ScrollStep
+    {
+        get => (double)GetValue(ScrollStepProperty);
+        set => SetValue(ScrollStepProperty, value);
+    }
+
+    public Ribbon()
+    {
+        DefaultStyleKey = typeof(Ribbon);
+
+        _items = [];
+        _items.CollectionChanged += OnItemsCollectionChanged;
+    }
+
+    public ICollection<UIElement> Items => _items;
+
+    protected override void OnApplyTemplate()
+    {
+        _panel = GetTemplateChild(PanelTemplatePart) as Panel;
+        if (_panel is not null)
+        {
+            foreach (var item in _items)
+            {
+                _panel.Children.Add(item);
+            }
+
+            _panel.SizeChanged += OnSizeChanged;
+        }
+
+        _decrementButton = GetTemplateChild(ScrollDecrementButtonTempatePart) as ButtonBase;
+        if (_decrementButton is not null)
+        {
+            _decrementButton.Click += OnDecrementScrollViewer;
+        }
+
+        _incrementButton = GetTemplateChild(ScrollIncrementButtonTempatePart) as ButtonBase;
+        if (_incrementButton is not null)
+        {
+            _incrementButton.Click += OnIncrementScrollViewer;
+        }
+
+        _scrollViewer = GetTemplateChild(ScrollViewerTemplatePart) as ScrollViewer;
+        if (_scrollViewer is not null)
+        {
+            _scrollViewer.ViewChanged += OnViewChanged;
+            _scrollViewer.SizeChanged += OnSizeChanged;
+            UpdateScrollButtonsState();
+        }
+    }
+
+    private void OnItemsCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (_panel is null)
+        {
+            return;
+        }
+
+        switch (e.Action)
+        {
+            case NotifyCollectionChangedAction.Add:
+                if (e.NewItems is not null)
+                {
+                    for (var i = 0; i < e.NewItems.Count; i++)
+                    {
+                        var element = (UIElement?)e.NewItems[i] ?? throw new ArgumentException("Item must not be null");
+                        _panel.Children.Insert(e.NewStartingIndex + i, element);
+                    }
+                }
+                break;
+            case NotifyCollectionChangedAction.Remove:
+                if (e.OldItems is not null)
+                {
+                    for (var i = 0; i < e.OldItems.Count; i++)
+                    {
+                        var element = (UIElement?)e.OldItems[i] ?? throw new ArgumentException("Item must not be null");
+                        _panel.Children.Insert(e.OldStartingIndex, element);
+                    }
+                }
+                break;
+            case NotifyCollectionChangedAction.Replace:
+                if (e.NewItems is not null)
+                {
+                    for (var i = 0; i < e.NewItems.Count; i++)
+                    {
+                        var element = (UIElement?)e.NewItems[i] ?? throw new ArgumentException("Item must not be null");
+                        _panel.Children[e.NewStartingIndex + i] = element;
+                    }
+                }
+                break;
+            case NotifyCollectionChangedAction.Move:
+                _panel.Children.Move((uint)e.OldStartingIndex, (uint)e.NewStartingIndex);
+                break;
+            case NotifyCollectionChangedAction.Reset:
+                _panel.Children.Clear();
+                if (e.NewItems is not null)
+                {
+                    foreach (var newItem in e.NewItems)
+                    {
+                        _panel.Children.Add((UIElement)newItem);
+                    }
+                }
+                break;
+            default:
+                throw new ArgumentException("Invalid value for NotifyCollectionChangedAction");
+        }
+    }
+
+    private void OnViewChanged(object? sender, ScrollViewerViewChangedEventArgs e) => UpdateScrollButtonsState();
+
+    private void OnSizeChanged(object? sender, SizeChangedEventArgs e) => UpdateScrollButtonsState();
+
+    private void UpdateScrollButtonsState()
+    {
+        if (_scrollViewer is null)
+        {
+            return;
+        }
+
+        if (_scrollViewer.ExtentWidth <= _scrollViewer.ViewportWidth)
+        {
+            VisualStateManager.GoToState(this, NoButtonsStateTemplatePart, useTransitions: true);
+            return;
+        }
+
+        var showDecrement = _scrollViewer.HorizontalOffset >= 1;
+        var showIncrement = _scrollViewer.ExtentWidth - _scrollViewer.HorizontalOffset - _scrollViewer.ViewportWidth >= 1;
+        if (showDecrement && showIncrement)
+        {
+            VisualStateManager.GoToState(this, BothButtonsStateTemplatePart, useTransitions: true);
+        }
+        else if (showDecrement)
+        {
+            VisualStateManager.GoToState(this, DecrementButtonStateTemplatePart, useTransitions: true);
+        }
+        else if (showIncrement)
+        {
+            VisualStateManager.GoToState(this, IncrementButtonStateTemplatePart, useTransitions: true);
+        }
+        else
+        {
+            VisualStateManager.GoToState(this, NoButtonsStateTemplatePart, useTransitions: true);
+        }
+    }
+
+    private void OnDecrementScrollViewer(object sender, RoutedEventArgs e)
+        => _scrollViewer?.ChangeView(_scrollViewer.HorizontalOffset - ScrollStep, verticalOffset: null, zoomFactor: null);
+
+    private void OnIncrementScrollViewer(object sender, RoutedEventArgs e)
+        => _scrollViewer?.ChangeView(_scrollViewer.HorizontalOffset + ScrollStep, verticalOffset: null, zoomFactor: null);
+}
