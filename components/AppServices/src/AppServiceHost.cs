49de3099cac4e2b8e9852ab8bfa511ed434ee591
Arlo Godfrey <arlo.godfrey@outlook.com>
2024-10-14 15:05:19 -0500

    Fixed invalid namespace reference

--- components/AppServices/src/AppServiceHost.cs
+++ components/AppServices/src/AppServiceHost.cs
@@ -75,7 +75,7 @@ public abstract class AppServiceHost
     private static bool CanUseAppServiceFunctionality { get; } = AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Desktop" && ApiInformation.IsApiContractPresent("Windows.ApplicationModel.FullTrustAppContract", 1, 0);
 
     /// <summary>
-    /// Handles the app service activation when <see cref="Windows.UI.Xaml.Application.OnBackgroundActivated(BackgroundActivatedEventArgs)"/> is invoked.
+    /// Handles the app service activation when <see cref="OnBackgroundActivated(BackgroundActivatedEventArgs)"/> is invoked.
     /// </summary>
     /// <param name="args">The args for the background activation.</param>
     /// <returns>Whether this activation was an app service connection that could be handled by this host.</returns>
