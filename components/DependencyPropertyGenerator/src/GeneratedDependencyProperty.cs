96578276d2af9734d052e4dbeb596ace8323d206
Arlo <arlo.godfrey@outlook.com>
2024-12-04 18:22:04 -0600

    Fixed compilation conditionals

--- components/DependencyPropertyGenerator/src/GeneratedDependencyProperty.cs
+++ components/DependencyPropertyGenerator/src/GeneratedDependencyProperty.cs
@@ -2,10 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#if WINDOWS_UWP || HAS_UNO
-using DependencyProperty = Windows.UI.Xaml.DependencyProperty;
-#else
+#if WINAPPSDK 
 using DependencyProperty = Microsoft.UI.Xaml.DependencyProperty;
+#else
+using DependencyProperty = Windows.UI.Xaml.DependencyProperty;
 #endif
 
 namespace CommunityToolkit.WinUI;
