072c750fa2dc179ffbcb550f7477c9f14b0991a5
Sergio Pedri <sergio0694@live.com>
2024-12-27 18:30:15 +0100

    Add 'AttributeInfo' type and associated logic

--- components/DependencyPropertyGenerator/CommunityToolkit.DependencyPropertyGenerator.SourceGenerators/Models/TypedConstantInfo.Factory.cs
+++ components/DependencyPropertyGenerator/CommunityToolkit.DependencyPropertyGenerator.SourceGenerators/Models/TypedConstantInfo.Factory.cs
@@ -3,11 +3,16 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Threading;
 using CommunityToolkit.GeneratedDependencyProperty.Extensions;
+using CommunityToolkit.GeneratedDependencyProperty.Helpers;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Operations;
 
 namespace CommunityToolkit.GeneratedDependencyProperty.Models;
 
@@ -65,11 +70,12 @@ partial record TypedConstantInfo
     }
 
     /// <summary>
-    /// Creates a new <see cref="TypedConstantInfo"/> instance from a given <see cref="TypedConstant"/> value.
+    /// Creates a new <see cref="TypedConstantInfo"/> instance from a given <see cref="IOperation"/> instance.
     /// </summary>
-    /// <param name="arg">The input <see cref="TypedConstant"/> value.</param>
+    /// <param name="operation">The input <see cref="IOperation"/> instance.</param>
     /// <returns>A <see cref="TypedConstantInfo"/> instance representing <paramref name="arg"/>.</returns>
     /// <exception cref="ArgumentException">Thrown if the input argument is not valid.</exception>
+    /// <remarks>This method only supports constant values.</remarks>
     public static bool TryCreate(IOperation operation, [NotNullWhen(true)] out TypedConstantInfo? result)
     {
         // Validate that we do have some constant value
@@ -112,5 +118,87 @@ partial record TypedConstantInfo
         };
 
         return true;
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="TypedConstantInfo"/> instance from a given <see cref="IOperation"/> instance.
+    /// </summary>
+    /// <param name="operation">The input <see cref="IOperation"/> instance.</param>
+    /// <param name="semanticModel">The <see cref="SemanticModel"/> that was used to retrieve <paramref name="operation"/>.</param>
+    /// <param name="expression">The <see cref="ExpressionSyntax"/> that <paramref name="operation"/> was retrieved from.</param>
+    /// <param name="token">The cancellation token for the current operation.</param>
+    /// <param name="result">The resulting <see cref="TypedConstantInfo"/> instance, if available.</param>
+    /// <returns>Whether a resulting <see cref="TypedConstantInfo"/> instance could be created.</returns>
+    /// <exception cref="ArgumentException">Thrown if the input argument is not valid.</exception>
+    public static bool TryCreate(
+        IOperation operation,
+        SemanticModel semanticModel,
+        ExpressionSyntax expression,
+        CancellationToken token,
+        [NotNullWhen(true)] out TypedConstantInfo? result)
+    {
+        if (TryCreate(operation, out result))
+        {
+            return true;
+        }
+
+        if (operation is ITypeOfOperation typeOfOperation)
+        {
+            result = new Type(typeOfOperation.TypeOperand.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat));
+
+            return true;
+        }
+
+        if (operation is IArrayCreationOperation)
+        {
+            string? elementTypeName = ((IArrayTypeSymbol?)operation.Type)?.ElementType.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+
+            // If the element type is not available (since the attribute wasn't checked), just default to object
+            elementTypeName ??= "object";
+
+            // Handle all possible ways of initializing arrays in attributes
+            IEnumerable<ExpressionSyntax>? arrayElementExpressions = expression switch
+            {
+                ImplicitArrayCreationExpressionSyntax { Initializer.Expressions: { } expressions } => expressions,
+                ArrayCreationExpressionSyntax { Initializer.Expressions: { } expressions } => expressions,
+                CollectionExpressionSyntax { Elements: { } elements } => elements.OfType<ExpressionElementSyntax>().Select(static element => element.Expression),
+                _ => null
+            };
+
+            // No element expressions found, just return an empty array
+            if (arrayElementExpressions is null)
+            {
+                result = new Array(elementTypeName, ImmutableArray<TypedConstantInfo>.Empty);
+
+                return true;
+            }
+
+            using ImmutableArrayBuilder<TypedConstantInfo> items = new();
+
+            // Enumerate all array elements and extract serialized info for them
+            foreach (ExpressionSyntax elementExpressions in arrayElementExpressions)
+            {
+                if (semanticModel.GetOperation(elementExpressions, token) is not IOperation initializationOperation)
+                {
+                    goto Failure;
+                }
+
+                if (!TryCreate(initializationOperation, semanticModel, elementExpressions, token, out TypedConstantInfo? elementInfo))
+                {
+                    goto Failure;
+                }
+
+                items.Add(elementInfo);
+            }
+
+            result = new Array(elementTypeName, items.ToImmutable());
+
+            return true;
+        }
+
+        Failure:
+        result = null;
+
+        return false;
     }
 }
