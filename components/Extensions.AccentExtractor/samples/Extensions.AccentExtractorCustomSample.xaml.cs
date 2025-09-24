ae8fe9defe10da88f909b52be45725213c4e573c
Adam Dernis <Avid29@Live.com>
2025-09-25 02:51:00 +0300

    AccentExtractor proof of concept pixel retrieval

--- components/Extensions.AccentExtractor/samples/Extensions.AccentExtractorCustomSample.xaml.cs
+++ components/Extensions.AccentExtractor/samples/Extensions.AccentExtractorCustomSample.xaml.cs
@@ -9,22 +9,11 @@ namespace Extensions.AccentExtractorExperiment.Samples;
 /// <summary>
 /// An example sample page of a custom control inheriting from Panel.
 /// </summary>
-[ToolkitSampleTextOption("TitleText", "This is a title", Title = "Input the text")]
-[ToolkitSampleMultiChoiceOption("LayoutOrientation", "Horizontal", "Vertical", Title = "Orientation")]
-
-[ToolkitSample(id: nameof(Extensions.AccentExtractorCustomSample), "Custom control", description: $"A sample for showing how to create and use a {nameof(Extensions.AccentExtractor)} custom control.")]
-public sealed partial class Extensions.AccentExtractorCustomSample : Page
+[ToolkitSample(id: nameof(AccentExtractorCustomSample), "Custom control", description: $"A sample for showing how ")]
+public sealed partial class AccentExtractorCustomSample : Page
 {
-    public Extensions.AccentExtractorCustomSample()
+    public AccentExtractorCustomSample()
     {
         this.InitializeComponent();
     }
-
-    // TODO: See https://github.com/CommunityToolkit/Labs-Windows/issues/149
-    public static Orientation ConvertStringToOrientation(string orientation) => orientation switch
-    {
-        "Vertical" => Orientation.Vertical,
-        "Horizontal" => Orientation.Horizontal,
-        _ => throw new System.NotImplementedException(),
-    };
 }
--- components/Extensions.AccentExtractor/src/AccentExtractor.cs
+++ components/Extensions.AccentExtractor/src/AccentExtractor.cs
@@ -2,31 +2,122 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.UI;
+using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml.Media.Imaging;
+using System.Buffers;
+using Windows.Graphics.Imaging;
+using Windows.UI;
 
 namespace CommunityToolkit.WinUI.Extensions;
 
+/// <summary>
+/// 
+/// </summary>
 public static partial class AccentExtractor
 {
     /// <summary>
-    /// Attached <see cref="DependencyProperty"/> for the <see cref="Image"/> that enables or disables accent color calculation.
+    /// Attached <see cref="DependencyProperty"/> that enables or disables accent color calculation.
     /// </summary>
-    public static readonly DependencyProperty ImageCalculateAccentProperty =
-        DependencyProperty.RegisterAttached("CalculateAccent", typeof(bool), typeof(Image), new PropertyMetadata(false, OnCalculateAccentChanged));
-    
+    public static readonly DependencyProperty CalculateAccentProperty =
+        DependencyProperty.RegisterAttached("CalculateAccent", typeof(bool), typeof(AccentExtractor), new PropertyMetadata(false, OnCalculateAccentChanged));
+
     /// <summary>
-    /// Attached <see cref="DependencyProperty"/> for the <see cref="ImageBrush"/> that enables or disables accent color calculation.
+    /// Attached <see cref="DependencyProperty"/> that holds the calculated accent color.
     /// </summary>
-    public static readonly DependencyProperty ImageBrushCalculateAccentProperty =
-        DependencyProperty.RegisterAttached("CalculateAccent", typeof(bool), typeof(ImageBrush), new PropertyMetadata(false, OnCalculateAccentChanged));
+    public static readonly DependencyProperty AccentColorProperty =
+        DependencyProperty.RegisterAttached("AccentColor", typeof(Color), typeof(AccentExtractor), new PropertyMetadata(Colors.Transparent));
+
+    /// <summary>
+    /// Gets a value indicating whether the accent calculation is enabled for the specified <see cref="UIElement"/>.
+    /// </summary>
+    /// <param name="obj">The <see cref="UIElement"/> from which to retrieve the value.</param>
+    /// <returns><see langword="true"/> if accent calculation is enabled for the specified <see cref="UIElement"/>; otherwise,
+    /// <see langword="false"/>.</returns>
+    public static bool GetCalculateAccent(UIElement obj) => (bool)obj.GetValue(CalculateAccentProperty);
+
+    /// <summary>
+    /// Sets a value indicating whether the accent calculation is enabled for the specified <see cref="UIElement"/>.
+    /// </summary>
+    /// <param name="obj">The <see cref="UIElement"/> on which to assign the value.</param>
+    /// <param name="value">The value to assign.</param>
+    public static void SetCalculateAccent(UIElement obj, bool value) => obj.SetValue(CalculateAccentProperty, value);
+
+    /// <summary>
+    ///  Gets the calculated accent color for the specified <see cref="UIElement"/>.
+    /// </summary>
+    /// <param name="obj">The <see cref="UIElement"/> from which to retrieve the value.</param>
+    /// <returns>The accent color calculated for the <see cref="UIElement"/>.</returns>
+    public static Color GetAccentColor(UIElement obj) => (Color)obj.GetValue(AccentColorProperty);
+
+    /// <summary>
+    /// Sets the calculated accent color for the specified <see cref="UIElement"/>.
+    /// </summary>
+    /// <remarks>
+    /// Can this be removed? The color is calculated automatically, so there's no need to set it manually.
+    /// </remarks>
+    /// <param name="obj"></param>
+    /// <param name="value"></param>
+    public static void SetAccentColor(UIElement obj, Color value) => obj.SetValue(AccentColorProperty, value);
+
+    /// <summary>
+    /// Updates the <see cref="AccentColorProperty"/> attached property based on the dominant color of the <see cref="UIElement"/>.
+    /// </summary>
+    /// <param name="sender">The <see cref="UIElement"/> to extract the accent color from.</param>
+    public static async Task UpdateAccentAsync(this UIElement sender)
+    {
+        // TODO: Properly remove Uno support
+#if !HAS_UNO
+        // Rerender the UIElement to a 64x64 bitmap
+        RenderTargetBitmap bitmap = new RenderTargetBitmap();
+        await bitmap.RenderAsync(sender, 64, 64);
+
+        // Create a stream from the bitmap
+        var pixels = await bitmap.GetPixelsAsync();
+        var stream = pixels.AsStream();
+
+        // 
+        if (stream.Length == 0)
+            return;
+
+        // Read the stream into a a color array
+        int pos = 0;
+        Span<Color> colors = new Color[(int)stream.Length / 4]; // This should be 4096 (64x64), but it's good to be safe.
+        Span<byte> bytes = stackalloc byte[4];
+        while (stream.Read(bytes) > 0)
+        {
+            // Safety check to avoid going out of bounds
+            // This should never happen, but it's good to be safe.
+            if (pos >= colors.Length)
+                break;
+
+            colors[pos] = Color.FromArgb(bytes[3], bytes[2], bytes[1], bytes[0]);
+            pos++;
+        }
+
+        var rand = Random.Shared.Next(colors.Length - 1);
+        var accent = colors[rand];
+
+        // Set the accent color on the UI thread
+        DispatcherQueue.GetForCurrentThread().TryEnqueue(() => SetAccentColor(sender, accent));
+#endif
+    }
 
     private static void OnCalculateAccentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
         // No change. Do nothing.
         if ((bool)e.NewValue == (bool)e.OldValue)
             return;
-        
+
+        // If true, calculate the accent color immediately.
         bool assign = (bool)e.NewValue;
+        if (assign && d is UIElement uie)
+        {
+            _ = uie.UpdateAccentAsync();
+        }
+        
+        // If the element is an Image or ImageBrush, register for the image opened event.
+        // This ensures that the accent color is recalculated when the image source changes.
         switch (d)
         {
             case Image image:
@@ -52,22 +143,6 @@ public static partial class AccentExtractor
         }
     }
 
-    private static void OnImageOpened(object sender, RoutedEventArgs e)
-    {
-        // Extract the ImageSource from the sender
-        ImageSource? source = null;
-        switch (sender)
-        {
-            case Image image:
-                source = image.Source;
-                break;
-            case ImageBrush imageBrush:
-                source = imageBrush.ImageSource;
-                break;
-        }
-
-        // If the source is not a BitmapSource, we cannot process it
-        if (source is not BitmapSource bitmapSource)
-            return;
-    }
+    private static async void OnImageOpened(object sender, RoutedEventArgs e) => await UpdateAccentAsync((UIElement)sender);
 }
+
